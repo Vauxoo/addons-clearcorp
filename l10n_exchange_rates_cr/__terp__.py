b100d8746b30c2987a791fde9f0fbd9884c6bda3
ClearCorp S.A. <info@clearcorp.co.cr>
2010-07-26 03:06:52 -0600

    Replacement of main company

---
+++ l10n_exchange_rates_cr/__terp__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    __terp__.py
+#    l10n_exchange_rates_cr
+#    Author: ClearCorp S.A.
+#    Copyright (C) 2010 ClearCorp S.A. (<http://www.clearcorp.co.cr>). All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    "name" : "Costa Rica generic exchange rates",
+    "version" : "0.1",
+    'category': 'General Modules',
+    "depends" : ['base'],
+    "author" : "ClearCorp",
+    "description": """Costa Rica exchange rates.
+    """,
+    'website': 'http://www.clearcorp.co.cr',
+    'init_xml': [],
+    'update_xml': ['exchange_rates_data.xml'],
+    'demo_xml': [],
+    'installable': True,
+    'active': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
