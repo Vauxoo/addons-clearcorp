b73cd7eb8c9c062aca49065f7c3dcfb4767f3453
Carlos Vásquez <carlos.vasquez@clearcorp.co.cr>
2010-08-03 10:31:17 -0600

    [REF] __init__.py, __terp__.py: added ClearCorp official header

--- ccorp_main_company/__terp__.py
+++ ccorp_main_company/__terp__.py
@@ -1,39 +1,51 @@
-# -*- coding: utf-8 -*-
+# -*- encoding: utf-8 -*-
 ##############################################################################
 #
 #    __terp__.py
-#    ccorp_main_company
-#    Author: ClearCorp S.A.
-#    Copyright (C) 2010 ClearCorp S.A. (<http://www.clearcorp.co.cr>). All Rights Reserved
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
+#    ccorp_seisa_main_company
+#    First author: Carlos Vásquez <carlos.vasquez@clearcorp.co.cr> (ClearCorp S.A.)
+#    Copyright (c) 2010-TODAY ClearCorp S.A. (http://clearcorp.co.cr). All rights reserved.
+#    
+#    Redistribution and use in source and binary forms, with or without modification, are
+#    permitted provided that the following conditions are met:
+#    
+#       1. Redistributions of source code must retain the above copyright notice, this list of
+#          conditions and the following disclaimer.
+#    
+#       2. Redistributions in binary form must reproduce the above copyright notice, this list
+#          of conditions and the following disclaimer in the documentation and/or other materials
+#          provided with the distribution.
+#    
+#    THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ``AS IS'' AND ANY EXPRESS OR IMPLIED
+#    WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+#    FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> OR
+#    CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+#    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+#    ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+#    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+#    ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#    
+#    The views and conclusions contained in the software and documentation are those of the
+#    authors and should not be interpreted as representing official policies, either expressed
+#    or implied, of ClearCorp S.A..
+#    
 ##############################################################################
 
-
 {
-    "name" : "Changes default main company to ClearCorp",
-    "version" : "0.1",
+    'name': 'Changes default main company to ClearCorp',
+    'version': '0.1',
+    'url': 'http://launchpad.net/openerp-costa-rica',
+    'author': 'ClearCorp S.A.',
+    'website': 'http://clearcorp.co.cr',
     'category': 'Generic Modules/Base',
-    "depends" : ['base','l10n_cr_base'],
-    "author" : "ClearCorp",
-    "description": "Changes default main company to ClearCorp",
-    'website': 'http://www.clearcorp.co.cr',
-    'init_xml': ['ccorp_main_company_data.xml'],
-    'update_xml': [],
+    'description': """
+    """,
+    'depends': ['base','l10n_cr_base'],
+    'init_xml': [],
     'demo_xml': [],
+    'update_xml': ['ccorp_main_company_data.xml'],
+    'license': 'Other OSI approved licence',
     'installable': True,
     'active': True,
 }
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
