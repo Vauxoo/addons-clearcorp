de4d30d361c3e2eb0b7017370e02c583303c778a
Carlos Vásquez <carlos.vasquez@clearcorp.co.cr>
2011-01-20 22:28:13 -0600

    [FIX] cc_account_fix to account_fix [FIX] official headers in account_fix

--- account_fix/account_fix.py
+++ account_fix/account_fix.py
@@ -1,3 +1,36 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    account_fix.py
+#    account_fix
+#    First author: Juan Felipe Muñoz <juan.munoz@clearcorp.co.cr> (ClearCorp S.A.)
+#	Copyright (c) 2011-TODAY ClearCorp S.A. (http://clearcorp.co.cr). All rights reserved.
+#	
+#	Redistribution and use in source and binary forms, with or without modification, are
+#	permitted provided that the following conditions are met:
+#	
+#	   1. Redistributions of source code must retain the above copyright notice, this list of
+#	      conditions and the following disclaimer.
+#	
+#	   2. Redistributions in binary form must reproduce the above copyright notice, this list
+#	      of conditions and the following disclaimer in the documentation and/or other materials
+#	      provided with the distribution.
+#	
+#	THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ``AS IS'' AND ANY EXPRESS OR IMPLIED
+#	WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+#	FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> OR
+#	CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+#	CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#	SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+#	ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+#	NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+#	ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#	
+#	The views and conclusions contained in the software and documentation are those of the
+#	authors and should not be interpreted as representing official policies, either expressed
+#	or implied, of ClearCorp S.A..
+#    
+##############################################################################
 
 import time
 from operator import itemgetter
@@ -10,13 +43,10 @@ from tools import config
 from account import invoice
 
 
-
-
 class fix_invoice(osv.osv):
 	_name = "account.invoice"
 	_inherit = "account.invoice"
-    
-    
+        
 	def action_move_create(self, cr, uid, ids, *args):
 		ait_obj = self.pool.get('account.invoice.tax')
 		cur_obj = self.pool.get('res.currency')
@@ -247,6 +277,3 @@ class fix_invoice(osv.osv):
 					
 		return True  
 fix_invoice()
-        
-
-
