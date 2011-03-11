94f839873bfe7b087a1d1f8b8536e0ebb6697721
Carlos Vásquez <carlos.vasquez@clearcorp.co.cr>
2011-03-11 14:37:14 -0600

    [FIX] ccorp_sale build

--- ccorp_sale/report/sale_order.py
+++ ccorp_sale/report/sale_order.py
@@ -43,7 +43,7 @@ class sale_order_ccorp(report_sxw.rml_parse):
         res = 0
         line_ids = self.pool.get('sale.order.line').search(self.cr, self.uid, [('order_id', '=', sale_order.id)])
         for line in line_ids:
-            line_info = self.pool.get('sale.order.line').browse(self.cr, self.uid, id, self.context.copy())
+            line_info = self.pool.get('sale.order.line').browse(self.cr, self.uid, line, self.context.copy())
             res += line_info.price_subtotal * line_info.discount / 100
         return res
 
