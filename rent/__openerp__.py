fd0fdf5fdbf3998d6d0e3552dba835705b6aa394
Mag Guevara Rivera <>
2011-08-08 13:29:13 -0600

    [FIX] modulo renta CICR

--- rent/__openerp__.py
+++ rent/__openerp__.py
@@ -1,11 +1,11 @@
 {
-	"name"       : "Rent Module",
-	"author"     : "Mag Guevara Rivera",
-	"version"    : "1.0",
-	"depends"    : ["base"],
-	"init_xml"   : [],
-	"update_xml" : ["rent_view.xml"],
-	"category"   : "Rent"
-	"active"     : False,
-	"instable"   : True,
+	"name"        : "Rent Module",
+	"author"      : "Mag Guevara Rivera",
+	"version"     : "1.0",
+	"depends"     : ["base"],
+	"init_xml"    : [],
+	"update_xml"  : ["rent_view.xml"],
+	"category"    : "Rent"
+	"active"      : False,
+	"instalable"  : True,
 }
