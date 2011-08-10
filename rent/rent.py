cd22d6b6a8863007cc5101553ba4d0ac1155a068
Mag Guevara Rivera <>
2011-08-10 14:31:22 -0600

    [IMP] modulo renta CICCR

--- rent/rent.py
+++ rent/rent.py
@@ -14,7 +14,7 @@ class rent_client(osv.osv):
 		#'client_lastname2' : fields.char('Second Lastname',size=15,required=True),
 		'client_birthdate' : fields.date('Birthdate',select=1,required=True),
 		'partner_id'        : fields.char('Id',size=10,required=True,help='Cedula del cliente'),
-		'client_location'   : fields.one2many(
+		'client_location'   : fields.one2many('rent.location','location_id','Location'),
 		#'client_canton': fields.related('address', 'client_canton', type='selection', string='Canton'),
 		#'client_district': fields.related('client_canton', 'client_district', type='selection', string='District'),
 	}
