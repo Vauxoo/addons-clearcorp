d35d5a39ae407a6583e688b377263ee6d23776f1
Mag Guevara Rivera <>
2011-08-10 11:32:58 -0600

    [FIX] modulo renta CICCR

---
+++ rent/data/constanst.py
@@ -0,0 +1,5 @@
+SJ_CANTON = (('San Jose','San Jose'), ('Escazu','Escazu'), 
+			('Desamparados','Desamparados'), ('Puriscal','Puriscal'),('Tarrazú','Tarrazú'),('Aserrí','Aserrí'),('Mora','Mora'),
+			('Goicoechea','Goicoechea'),('Santa Ana','Santa Ana'),('Alajuelita','Alajuelita'),('Vázquez de Coronado','Vázquez de Coronado'),
+			('Acosta','Acosta'),('Tibás','Tibás'),('Moravia','Moravia'),('Montes de Oca','Montes de Oca'),('Turrubares','Turrubares'),('Dota','Dota'),
+			('Curridabat','Curridabat'),('Pérez Zeledón','Pérez Zeledón'),('León Cortés','León Cortés'))
