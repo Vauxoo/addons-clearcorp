bcbce2190f863199699fc7dbbb2dbd27a509aa4e
Mag Guevara Rivera <>
2011-08-10 11:38:03 -0600

    [FIX] modulo renta CICCR

--- rent/constanst.py
+++ rent/constanst.py
@@ -1,5 +1,5 @@
 SJ_CANTON = (('San Jose','San Jose'), ('Escazu','Escazu'), 
-			('Desamparados','Desamparados'), ('Puriscal','Puriscal'),('Tarrazú','Tarrazú'),('Aserrí','Aserrí'),('Mora','Mora'),
-			('Goicoechea','Goicoechea'),('Santa Ana','Santa Ana'),('Alajuelita','Alajuelita'),('Vázquez de Coronado','Vázquez de Coronado'),
-			('Acosta','Acosta'),('Tibás','Tibás'),('Moravia','Moravia'),('Montes de Oca','Montes de Oca'),('Turrubares','Turrubares'),('Dota','Dota'),
-			('Curridabat','Curridabat'),('Pérez Zeledón','Pérez Zeledón'),('León Cortés','León Cortés'))
+			('Desamparados','Desamparados'), ('Puriscal','Puriscal'),('Tarrazu','Tarrazu'),('Aserri','Aserri'),('Mora','Mora'),
+			('Goicoechea','Goicoechea'),('Santa Ana','Santa Ana'),('Alajuelita','Alajuelita'),('Vazquez de Coronado','Vazquez de Coronado'),
+			('Acosta','Acosta'),('Tibas','Tibas'),('Moravia','Moravia'),('Montes de Oca','Montes de Oca'),('Turrubares','Turrubares'),('Dota','Dota'),
+			('Curridabat','Curridabat'),('Perez Zeledon','Perez Zeledon'),('Leon Cortes','Leon Cortes'))
