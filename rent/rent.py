e6bcad6b20faea624d09c50d3fa757be82bdab9a
Mag Guevara Rivera <>
2011-08-10 10:52:48 -0600

    [IMP] modulo renta CICCR

--- rent/rent.py
+++ rent/rent.py
@@ -37,7 +37,7 @@ class rent_state(osv.osv):
 		obj = self.browse(cr,uid,ids)
 		sj_canton = 0
 		v ['state_canton'] = {
-			'San Jose' : (('San Jose','San Jose'), ('Escazu','Escazu'),('Desamparados','Desamparados'), ('Puriscal','Puriscal'),('Tarrazú','Tarrazú'),('Aserrí','Aserrí'),('Mora','Mora'), ('Goicoechea','Goicoechea'),('Santa Ana','Santa Ana'),('Alajuelita','Alajuelita'),('Vázquez de Coronado','Vázquez de Coronado'),('Acosta','Acosta'),('Tibás','Tibás'),('Moravia','Moravia'),('Montes de Oca','Montes de Oca'),('Turrubares','Turrubares'),('Dota','Dota'),('Curridabat','Curridabat'),('Pérez Zeledón','Pérez Zeledón'),('León Cortés','León Cortés')),
+			'San Jose' : (('San Jose','San Jose')),
 			'Heredia'  : (), 
 		}[value](pField)
 		
