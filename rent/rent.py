999c682a6ba939ad2229cd5be8a474012bcb58e7
Mag Guevara Rivera <>
2011-08-11 11:08:36 -0600

    [FIX] modulo renta CICCR

--- rent/rent.py
+++ rent/rent.py
@@ -12,11 +12,11 @@ class rent_client(osv.osv):
 	_columns = {
 		#'client_lastname'  : fields.char('Lastname',size=15,required=True),
 		#'client_lastname2' : fields.char('Second Lastname',size=15,required=True),
-		'client_birthdate' : fields.date('Birthdate',select=1,required=True),
 		#'partner_id'        : fields.char('Id',size=10,required=True,help='Cedula del cliente'),
+		'client_birthdate' : fields.date('Birthdate',select=1,required=True),		
 		'client_location'   : fields.one2many('rent.location','location_id','Location'),
-		#'client_canton': fields.related('address', 'client_canton', type='selection', string='Canton'),
-		#'client_district': fields.related('client_canton', 'client_district', type='selection', string='District'),
+		'client_canton': fields.related('address', 'client_canton', type='selection', string='Canton'),
+		'client_district': fields.related('client_canton', 'client_district', type='selection', string='District'),
 	}
 rent_client()
 
