fce66c250b819d1277165b02dc9ec487ba6eee48
Mag Guevara Rivera <>
2011-08-11 15:30:02 -0600

    [IMP] modulo renta CICCR

--- rent/rent.py
+++ rent/rent.py
@@ -62,7 +62,7 @@ class rent_state(osv.osv):
 	_rec_name = "state_number"
 	_columns = {
 		#'state_province' : fields.selection((('Alajuela', 'Alajuela'),('Cartago','Cartago'),('Guanacaste','Guanacaste'),('Heredia','Heredia'),
-											('Limon', 'Limon'),('San Jose', 'San Jose'),('Puntarenas', 'Puntarenas')),'Province', required=True),
+		#									('Limon', 'Limon'),('San Jose', 'San Jose'),('Puntarenas', 'Puntarenas')),'Province', required=True),
 		
 		'state_canton'    : fields.related('address', 'location_canton', type='char', string='Canton'),
 		'state_district'  : fields.related('address', 'location_district', type='char', string='District'),
