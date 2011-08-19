6351409d14ea2a604ace6726a844208b9a0012d0
Mag Guevara Rivera <>
2011-08-19 15:01:32 -0600

    [IMP] modulo de rentas CICCR

--- rent/rent.py
+++ rent/rent.py
@@ -48,8 +48,8 @@ class rent_location(osv.osv):
 		canton_ids = self.pool.get('rent.canton').search(cr,uid,[('state_id','=',obj_state.name)])
 		debug(canton_ids)
 		for canton in canton_ids:
-			obj = self.pool.get('rent.canton').browse(cr,uid,canton)
-			canton_list.append(obj)
+			obj_canton = self.pool.get('rent.canton').browse(cr,uid,canton)
+			canton_list.append((obj_canton.code,obj_canton.name))
 			debug(canton_list)
 		v['canton_id'] = canton_list
 		return {
