6fd2cf7d3fd32d39e4418c2c67a6334fbeeced6e
Mag Guevara Rivera <>
2011-08-23 11:08:19 -0600

    [IMP] modulo de renta CICCR

--- rent/rent.py
+++ rent/rent.py
@@ -214,7 +214,7 @@ class rent_contract(osv.osv):
 	_columns = {
 		'name'             : fields.char('Reference', size=64),
 		'contract_rent'    : fields.many2one('rent.rent','Rent Reference'),
-		'contract_clauses' : fields.many2many('rent.contract.clause','rent_contract_clause_rel','name','clause_code','Clausulas'),
+		'contract_clauses' : fields.many2many('rent.contract.clause','rent_contract','name','clause_code','Clausulas'),
 		'contract_design'  : fields.char('Design',size=64,required=True),
 	}
 rent_contract()
