7c03329a336968913b9f49226c6b0d40748c92d6
Mag Guevara Rivera <>
2011-08-25 15:17:25 -0600

    [IMP] modulo de renta CICCR

--- rent/rent.py
+++ rent/rent.py
@@ -233,8 +233,8 @@ class rent_rent(osv.osv):
 				debug(obj_rent.rent_rent_local)
 				for obj_local in obj_rent.rent_rent_local:
 		#			res[rent_id] += obj_local.local_value 
-					total += obj_local._local_value(obj_local.id,None,None)
-					debug(total)
+					debug(obj_local._local_value(obj_local.id,None,None))
+					#debug(total)
 			elif obj_rent.rent_is_parking:
 				debug("PARQUEO")
 				obj_ids = obj_rent.rent_rent_parking
