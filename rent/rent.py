2d8264ba67e93a6cbaedf7016635f99a2e8e42fb
Mag Guevara Rivera <>
2011-08-30 14:36:50 -0600

    [FIX] arreglo de relacion con edificio desde el local revision

--- rent/rent.py
+++ rent/rent.py
@@ -213,7 +213,7 @@ class rent_floor_local(osv.osv):
 		'local_floor'              : fields.many2one('rent.floor','# Floor'),
 		#'local_local_by_floor'     : fields.one2many('rent.floor.floor','# Floor'),
 		#'local_floor'              : fields.related('rent.local.floor','# Floor'),
-		'local_building'           : fields.function(_get_building_local,type='many2one',method=True,string='Building'),
+		'local_building'           : fields.function(_get_building_local,type='char',method=True,string='Building'),
 		'local_gallery_photo'      : fields.char('Gallery of Images', size=64),
 		'local_photo'              : fields.binary('Main photo'),
 	}
