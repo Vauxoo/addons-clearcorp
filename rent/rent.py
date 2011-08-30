1af9c86465a94917bf412719e10b269b00e7f720
Mag Guevara Rivera <>
2011-08-30 15:43:20 -0600

    [FIX] arreglo determinacion de estado FINAl

--- rent/rent.py
+++ rent/rent.py
@@ -281,6 +281,19 @@ class rent_rent(osv.osv):
 		debug('+==================================')
 		
 		return res
+	def _calculate_years(self,cr,uid,ids,field_name,args,context):
+		debug('+==================================')
+		res = {}
+		for rent_id in ids:
+			obj_rent = self.pool.get('rent.rent').browse(cr,uid,rent_id)
+			if (obj_rent.rent_end_date != '' and  obj_rent.rent_start_date != ''):
+				fin = parser.parse(obj_rent.rent_end_date)
+				inicio = parser.parse(obj_rent.rent_start_date)
+				debug(inicio)
+				debug(fin)
+				res[rent_id] = (fin.year - inicio.year)
+			debug(res)
+		return res
 	def rent_valid(self,cr,uid,ids,context=None):
 		debug('BOTON====================================')
 		debug(ids)
