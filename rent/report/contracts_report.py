2d0dc3010e7190294a1837d545fa2b7d3fca5893
Mag Guevara Rivera <>
2011-09-01 13:09:25 -0600

    [FIX] mejora del archivo de contratos

--- rent/report/contracts_report.py
+++ rent/report/contracts_report.py
@@ -1,10 +1,10 @@
 import time
 from report import report_sxw
 
-class contracts_report(report_sxw.rml_parse):
+class contract_report(report_sxw.rml_parse):
 	def __init__(self,cr,uid,name,context):
-		super(contracts_report,self).__init__(cr,uid,name,context)
+		super(contract_report,self).__init__(cr,uid,name,context)
 		self.localcontext.update({
 			'time' : time,
 		})
-report_sxw.report_sxw('report.contracts_report','rent.contract', 'addons/rent/report/contracts.rml', parser=contracts_report,header=True)
+report_sxw.report_sxw('report.contract_report','rent_contract', 'addons/rent/report/contracts.rml', parser=contract_report,header=True)
