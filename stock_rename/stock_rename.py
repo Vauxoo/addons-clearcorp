197b80300ee4fe50ded0c29011ce1b827e7da904
Mag Guevara <mag.guevara@clearcorp.co.cr>
2011-09-21 11:23:32 -0600

    [FIX] obj_stock_location was no used to return the id

--- stock_rename/stock_rename.py
+++ stock_rename/stock_rename.py
@@ -37,7 +37,7 @@ class stock_location(osv.osv):
 			debug("DATA FINAL")
 			data = '/'.join(data)
 			debug(data)
-			res.append((location.id, data))  
+			res.append((obj_stock_location.id, data))  
 		return res
 	
 	def _complete_name2(self, cr, uid, ids, name, args, context=None):
