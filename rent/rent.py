393e52fd6e53cb142f39ecd6beec7f036dbabc56
Mag Guevara Rivera <>
2011-10-26 10:33:57 -0600

    [FIX] not invoicing every month fix invoice_day error calculation

--- rent/rent.py
+++ rent/rent.py
@@ -707,7 +707,7 @@ class rent_rent(osv.osv):
 			if type == 'rent':
 					invoice_day = (obj_rent.rent_invoiced_day <= obj_rent.rent_charge_day and obj_rent.rent_charge_day - obj_rent.rent_invoiced_day or calendar.mdays[today.month] - obj_rent.rent_charge_day + obj_rent.rent_invoiced_day + 1)
 			elif type == 'main':
-					invoice_day = (obj_rent.rent_main_invoiced_day <= rent_main_charge_day and obj_rent.rent_main_charge_day - obj_rent.rent_main_invoiced_day or calendar.mdays[today.month] - obj_rent.rent_main_invoiced_day + obj_rent.rent_main_charge_day + 1)
+					invoice_day = (obj_rent.rent_main_invoiced_day <= obj_rent.rent_main_charge_day and obj_rent.rent_main_charge_day - obj_rent.rent_main_invoiced_day or calendar.mdays[today.month] - obj_rent.rent_main_invoiced_day + obj_rent.rent_main_charge_day + 1)
 			debug(today)
 			debug(invoice_day)
 			if today.day == invoice_day:
