09eec61d596f3af279d35d4628edb2d8206103d2
Mag Guevara <mag.guevara@clearcorp.co.cr>
2011-11-03 11:24:02 -0600

    [ADD] rent model with currency exchange

---
+++ rent/__openerp__.py
@@ -0,0 +1,16 @@
+{
+	"name"        : "Rent",
+	"author"      : "Clear Corp S.A.",
+	"version"     : "1.0",
+	"depends"     : ["base","account_asset"],
+	"init_xml"    : [],
+	"update_xml"  : ["rent_view.xml", 
+					"rent_data.xml",
+					"rent_workflow.xml", 
+					'rent_contract_report.xml',
+					'security/rent_security.xml',
+					'security/ir.model.access.csv',],
+	"category"    : "Rent",
+	"active"      : False,
+	"instalable"  : True,
+}
