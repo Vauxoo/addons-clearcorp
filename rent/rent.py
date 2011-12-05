aa7ba6c9754716a99af81dff0defaa47d4e7d127
Mag Guevara <mag.guevara@clearcorp.co.cr>
2011-12-05 15:16:11 -0600

    [FIX] rent: class reference fix

--- rent/rent.py
+++ rent/rent.py
@@ -505,9 +505,27 @@ class rent_floor_parking(osv.osv):
 	]
 rent_floor_parking()
 
+class rent_rent_group(osv.osv):
+	_name = 'rent.rent.group'
+	
+	def create(self,cr,uid,vals,context=None):
+		if vals:
+			next_seq = self.pool.get('ir.sequence').get(cr, uid, 'rent.rent.group')
+			rent = vals.get('obj_rent',False)
+			o = self.pool.get('rent.rent').browse(cr,uid,rent)
+			code = next_seq or (o and ('GRP-' + (o.rent_related_real == 'local' and o.rent_rent_local_id.name_get() or (o.rent_related_real == 'estate' and o.rent_rent_estate_id.name_get() or (o.rent_related_real == 'parking' and o.rent_rent_parking_id.name_get() or '')))))
+			vals['code'] = code
+		return super(rent_rent_group,self).create(cr,uid,vals,context)
+	
+	_columns = {
+		'name'            : fields.char('Name',size=64,required=True),
+		'rent_rent_ids'   : fields.one2many('rent.rent','rent_group_id','Rents Members',readonly=True, domain=[('rent_type','=','Contract')]),
+	}
+rent_rent_group()
+
 #Class to hold all the information that refences the rent
 #value, dates, status and to control de transaction of the bussines
-#
+
 class rent_rent(osv.osv):
 	_name = 'rent.rent'
 	
@@ -1486,24 +1504,6 @@ class rent_rent(osv.osv):
 		'active': 1,
 	}
 rent_rent()
-
-class rent_rent_group(osv.osv):
-	_name = 'rent.rent.group'
-	
-	def create(self,cr,uid,vals,context=None):
-		if vals:
-			next_seq = self.pool.get('ir.sequence').get(cr, uid, 'rent.rent.group')
-			rent = vals.get('obj_rent',False)
-			o = self.pool.get('rent.rent').browse(cr,uid,rent)
-			code = next_seq or (o and ('GRP-' + (o.rent_related_real == 'local' and o.rent_rent_local_id.name_get() or (o.rent_related_real == 'estate' and o.rent_rent_estate_id.name_get() or (o.rent_related_real == 'parking' and o.rent_rent_parking_id.name_get() or '')))))
-			vals['code'] = code
-		return super(rent_rent_group,self).create(cr,uid,vals,context)
-	
-	_columns = {
-		'name'            : fields.char('Name',size=64,required=True),
-		'rent_rent_ids'   : fields.one2many('rent.rent','rent_group_id','Rents Members',readonly=True, domain=[('rent_type','=','Contract')]),
-	}
-rent_rent_group()
 
 class rent_rise_estimate(osv.osv):
 	_name = 'rent.rise.estimate'
