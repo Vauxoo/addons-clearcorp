49d2032bdc7f9bfae241932c75972a5974f2f54c
Mag Guevara <mag.guevara@clearcorp.co.cr>
2011-12-07 00:40:19 -0600

    [IMP] rent.rent: cron job for deposit test

--- rent/rent.py
+++ rent/rent.py
@@ -1228,17 +1228,19 @@ class rent_rent(osv.osv):
 		res_deposit_fix = []
 		debug("ENTRAAAAAAAAAA")
 		for obj_rent in self.browse(cr,uid,test_ids):
-			res_deposit_fix.append(({'rent_id':obj_rent.id,'current_amount':obj_rent.rent_amount_base,'deposit':obj_rent.rent_deposit}))
+			res_deposit_fix.append({'rent_id':obj_rent.id,'current_amount':obj_rent.rent_amount_base,'deposit':obj_rent.rent_deposit})
+			res_deposit_fix.append({'rent_id':obj_rent.id,'current_amount':obj_rent.rent_amount_base,'deposit':obj_rent.rent_deposit})
 		debug(res_deposit_fix)
 		self._check_deposit(cr,uid,test_ids,context=context)
 		return True
 	
 	def _check_deposit(self,cr,uid,args,context=None):
 		required_act = []
+		debug(args)
 		for record in args:
 			debug(record)
-			current = float(record['current_amount'])
-			depo = float(record['deposit'])
+			current = float(args['current_amount'])
+			depo = float(args['deposit'])
 			if current > depo:
 				required_act.append(rent_id)
 		debug(required_act)
