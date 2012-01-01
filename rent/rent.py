3509a02be1e9cc8aed8f74cae2238be803d5d975
mag-guevara <mag.guevara@clearcorp.co.cr>
2012-01-01 17:35:24 -0600

    [FIX] rent: change account for the invoice, using the account of the rent because the cannot use the one of the partner

--- rent/rent.py
+++ rent/rent.py
@@ -907,7 +907,8 @@ class rent_rent(osv.osv):
 			il.append(self.inv_line_create(cr, uid,obj_rent,rlist,type))
 
 		obj_client = obj_rent.rent_rent_client_id
-		a = obj_client.property_account_receivable.id
+		a = obj_rent.rent_rent_account_id or obj_client.property_account_receivable.id
+		#a = obj_client.property_account_receivable.id
 		journal_ids = journal_obj.search(cr, uid, [('type', '=','sale'),('company_id', '=',obj_rent.company_id.id)],limit=1)
 
 		if not journal_ids:
@@ -1104,7 +1105,8 @@ class rent_rent(osv.osv):
 				il.append(self.inv_line_create(cr, uid,obj_rent,rlist,type))
 
 		obj_client = obj_rent.rent_rent_client_id
-		a = obj_client.property_account_receivable.id
+		a = obj_rent.rent_rent_account_id or obj_client.property_account_receivable.id
+		#a = obj_client.property_account_receivable.id
 		journal_ids = journal_obj.search(cr, uid, [('type', '=','sale'),('company_id', '=',obj_rent.company_id.id)],limit=1)
 
 		if not journal_ids:
