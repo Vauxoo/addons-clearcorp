ae293e33ef132fe8f04c803bfbe785adf4f5b14d
mag-guevara <mag.guevara@clearcorp.co.cr>
2012-01-03 09:29:16 -0600

    [ADD] ccorp_payment_receipt

---
+++ ccorp_payment_receipt/report/payment_receipt.mako
@@ -0,0 +1,57 @@
+<html>
+<head>
+	<style style="text/css">
+		${css}
+	</style>
+</head>
+<body class = "data">
+	%for check in objects :
+	<% setLang(check.partner_id.lang) %>
+	<div id="wrapper">
+		<div id = "document_data">
+			<div id = "date">
+				<div id="number">${_("No.")}  ${check.number or ''|entity}</div>
+				<div class="text_font">${check.date or ''|entity}</div>
+			</div>
+			<div class = "detail">
+				<div class = "detail_line">
+					${_("We recieve from")}: <span class="text_font">${check.partner_id.name or ''|entity}</span>
+				</div>
+			</div>
+			<div class = "detail">
+				${_("The amount of")}: <span class = "amount_text">${get_text(check.amount,check.currency_id) or ''|entity}</span>
+			</div>
+			<div class = "detail">
+				${_("Concept of")}: <span class="text_font">${check.name or ''|entity}</span>
+			</div>
+			<div class = "detail">
+				${_("Observations")}: <span class = "text_font"> ${check.narration or ''|entity}</span>
+			</div>
+		</div>
+		<div id = "validation">
+			<div>
+				<div  class = "payment_method">
+					<hr/>
+				</div>
+				<div class = "payment_method">
+				%if check.journal_id.type == 'cash':
+					<span class = "amount">${_("Cash")}
+				%elif check.journal_id.type == 'bank':
+					<span class = "amount">${_("Check No.")}: <span class = "amount">${check.reference or ''|entity }</span>
+				%endif
+				</div>
+			</div>
+			<div id = "info">
+				<span class = "info_detail">${_("LA VALIDEZ DE ESTE RECIBO QUEDA SUJETA A QUE LOS CHEQUES RECIBIDOS SEAN PAGADOS POR EL BANCO A NUESTRA SATISFACCION")}</span>
+				<span class = "info_detail">${_("AUTORIZADO MEDIANTE OFICIO No.04-00007-97 DE FECHA 30-09-97 DE LA D.G.T.D.")}</span>
+			</div>
+			<div id = "signature">
+				<span class = "sign_detail">${_("P/")} ${check.company_id.partner_id.name or ''|entity }</span>
+				<hr/>
+			</div>
+		</div>
+	</div>
+	<p style="page-break-after:always"></p>
+%endfor
+</body>
+</html>
---
+++ ccorp_payment_receipt/report/payment_receipt.py
@@ -0,0 +1,59 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#
+#    payment_receipt
+#    First author: Mag Guevara <mag.guevara@clearcorp.co.cr> (ClearCorp S.A.)
+#    Copyright (c) 2010-TODAY ClearCorp S.A. (http://clearcorp.co.cr). All rights reserved.
+#    
+#    Redistribution and use in source and binary forms, with or without modification, are
+#    permitted provided that the following conditions are met:
+#    
+#       1. Redistributions of source code must retain the above copyright notice, this list of
+#          conditions and the following disclaimer.
+#    
+#       2. Redistributions in binary form must reproduce the above copyright notice, this list
+#          of conditions and the following disclaimer in the documentation and/or other materials
+#          provided with the distribution.
+#    
+#    THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ``AS IS'' AND ANY EXPRESS OR IMPLIED
+#    WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+#    FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> OR
+#    CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+#    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+#    ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+#    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+#    ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#    
+#    The views and conclusions contained in the software and documentation are those of the
+#    authors and should not be interpreted as representing official policies, either expressed
+#    or implied, of ClearCorp S.A..
+#    
+##############################################################################
+
+import time
+import pooler
+from report import report_sxw
+from amount_to_text import number_to_text_es
+import locale
+
+class payment_receipt(report_sxw.rml_parse):
+    def __init__(self, cr, uid, name, context):
+        super(payment_receipt, self).__init__(cr, uid, name, context=context)
+        self.localcontext.update({
+            'time': time,
+            'cr' : cr,
+            'uid': uid,
+            'get_text':self.get_text,
+        })
+            
+    def get_text(self,amount,currency):
+		res = number_to_text_es(amount,currency)
+		return res
+		
+report_sxw.report_sxw(
+    'report.account.voucher.layout_ccorp',
+    'account.voucher',
+    'addons/ccorp_payment_receipt/report/payment_receipt.mako',
+    parser=payment_receipt)
