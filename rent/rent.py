a4731b6664c93aa80b5b499eb8e23f54f8332d40
mag-guevara <mag.guevara@clearcorp.co.cr>
2012-01-09 14:53:06 -0600

    [IMP] rent.rent: first rent calculation method

--- rent/rent.py
+++ rent/rent.py
@@ -1314,34 +1314,37 @@ class rent_rent(osv.osv):
 	def action_first_invoice(self,cr,uid,ids,context=None):
 		#gets the list of all active rents
 		debug(ids)
-		rent_ids = self.search(cr,uid,[('state','=','active'), ('rent_type','in',['Contract'])])
+		#rent_ids = self.search(cr,uid,[('state','=','active'), ('rent_type','in',['Contract'])])
 		#is_required = self._invoice_required(cr,uid,rent_ids)
 		res_first_inv = []
 		res_first_main_inv = []
-		for obj_rent in self.browse(cr,uid,rent_ids):
+		for obj_rent in self.browse(cr,uid,ids):
 			#if is_required[obj_rent.id]: 
-			has_first = self.pool.get('rent.invoice.rent').search(cr,uid,[('invoice_rent_id','=',obj_rent.id),('invoice_type','=','rent')])
-			if not has_first and parser.parse(obj_rent.rent_start_date).date().month == date.today().month:
-				#res_first_inv.append(obj_rent.id)
-				if obj_rent.rent_type != "Adendum":
-					#we only create invoice for the contracts NOT for the adendums
-					res_first_inv.append(obj_rent)
-				percentaje = obj_rent.rent_performance.split('%')[0]
-				#we update the estimates list for the obj
-				obj_rent.write({'rent_estimates_ids' : [(0,0,{'estimate_performance': float(percentaje),'estimate_rent_id':obj_rent.id,'estimate_date' : date.today(), 'estimate_state':'final'})]})
-				
-			#We check for maintenance invoice for this we need to heck if the rent hasta a maintenance record
-			if obj_rent.rent_main_inc:
-				has_main_first = self.pool.get('rent.invoice.rent').search(cr,uid,[('invoice_rent_id','=',obj_rent.id),('invoice_type','=','main')])
-				if not has_main_first and parser.parse(obj_rent.rent_main_start_date).date().month == date.today().month:
+			if obj_rent.rent_type == 'Contract':
+				has_first = self.pool.get('rent.invoice.rent').search(cr,uid,[('invoice_rent_id','=',obj_rent.id),('invoice_type','=','rent')])
+				if not has_first and parser.parse(obj_rent.rent_start_date).date().month == date.today().month:
+					#res_first_inv.append(obj_rent.id)
 					if obj_rent.rent_type != "Adendum":
 						#we only create invoice for the contracts NOT for the adendums
-						res_first_main_inv.append(obj_rent)
-					percentaje = obj_rent.rent_main_performance.split('%')[0]
-					obj_rent.write({'rent_main_estimates_ids' : [(0,0,{'estimate_performance': float(percentaje),'estimate_rent_id':obj_rent.id,'estimate_date' : date.today(), 'estimate_state':'final'})]})
+						res_first_inv.append(obj_rent)
+					percentaje = obj_rent.rent_performance.split('%')[0]
+					#we update the estimates list for the obj
+					obj_rent.write({'rent_estimates_ids' : [(0,0,{'estimate_performance': float(percentaje),'estimate_rent_id':obj_rent.id,'estimate_date' : date.today(), 'estimate_state':'final'})]})
+					
+				#We check for maintenance invoice for this we need to heck if the rent hasta a maintenance record
+				if obj_rent.rent_main_inc:
+					has_main_first = self.pool.get('rent.invoice.rent').search(cr,uid,[('invoice_rent_id','=',obj_rent.id),('invoice_type','=','main')])
+					if not has_main_first and parser.parse(obj_rent.rent_main_start_date).date().month == date.today().month:
+						if obj_rent.rent_type != "Adendum":
+							#we only create invoice for the contracts NOT for the adendums
+							res_first_main_inv.append(obj_rent)
+						percentaje = obj_rent.rent_main_performance.split('%')[0]
+						obj_rent.write({'rent_main_estimates_ids' : [(0,0,{'estimate_performance': float(percentaje),'estimate_rent_id':obj_rent.id,'estimate_date' : date.today(), 'estimate_state':'final'})]})
 		
-		self.first_rent(cr,uid,res_first_inv)
-		self.first_rent(cr,uid,res_first_main_inv,'main')
+		if res_first_inv:
+			self.first_rent(cr,uid,res_first_inv)
+		if res_first_main_inv:
+			self.first_rent(cr,uid,res_first_main_inv,'main')
 		return {}
 	
 	def calculate_negotiation(self,cr,uid,ids,context=None):
