d858e6e0e38dc979f2e6c456f30f9e29ccbe27a2
mag-guevara <mag.guevara@clearcorp.co.cr>
2012-01-09 13:42:01 -0600

    [FIX] rent.rent: aprove adendum wrong search criteria

--- rent/rent.py
+++ rent/rent.py
@@ -1282,7 +1282,7 @@ class rent_rent(osv.osv):
 		return True
 		
 	def action_aprove_adendum(self,cr,uid,ids,context=None):
-		rent_ids = self.search(cr,uid,[('state','=','active'), ('rent_type','in',['Adendum','Others']),('rent_modif_date','=','NULL')])
+		rent_ids = self.search(cr,uid,[('state','=','active'), ('rent_type','in',['Adendum','Others']),('rent_modif_date','=',False)])
 		debug(rent_ids)
 		for rent_aden_id in rent_ids:
 			vals = self.copy_data(cr,uid,rent_aden_id)
