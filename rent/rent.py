de876112cd2ee394237ed18a19e8033914809343
Mag Guevara <mag.guevara@clearcorp.co.cr>
2012-08-29 00:02:07 -0600

    [FIX] sending object instead of id register_historic

--- rent/rent.py
+++ rent/rent.py
@@ -519,7 +519,7 @@ class rent_rent(osv.osv):
 			#obj_rent.write({'rent_amount_base' : years_val})
 			vals['rent_amount_base'] = years_val
 			if obj_rent.rent_related_real == 'local':
-				vals['anual_value_local'] = obj_rent.rent_rent_local
+				vals['anual_value_local'] = obj_rent.rent_rent_local.id
 			if not is_registrated:
 				vals['rent_historic'] = [(0,0,{'anual_value_rent':obj_rent.id,'anual_value_value':years_val,'anual_value_prev_value' : prev_value,'anual_value_rate' : obj_rent.rent_rise, 'anual_value_date' : current_date, 'anual_value_type' : 'rent', 'anual_value_local':vals['anual_value_local']})]
 			else:
@@ -554,7 +554,7 @@ class rent_rent(osv.osv):
 			#obj_rent.write({'rent_amount_base' : years_val})
 			vals['rent_main_amount_base'] = years_val
 			if obj_rent.rent_related_real == 'local':
-				vals['anual_value_local'] = obj_rent.rent_rent_local
+				vals['anual_value_local'] = obj_rent.rent_rent_local.id
 			if not is_registrated:
 				vals['rent_main_historic_ids'] = [(0,0,{'anual_value_rent':obj_rent.id,'anual_value_value':years_val,'anual_value_prev_value' : prev_value,'anual_value_rate' : rise, 'anual_value_date' : current_date, 'anual_value_type' : 'main','anual_value_local':vals['anual_value_local']})]
 			else:
