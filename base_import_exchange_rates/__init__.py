09271f190e4d79cce2696e1b0d68cd77a597f9fb
Miguel Gutiérrez <miguel.gutierrez@siesacr.com>
2013-04-01 11:37:08 -0600

    [FIX] Fix base currency import module to import more than 2 currency

--- base_import_exchange_rates/__init__.py
+++ base_import_exchange_rates/__init__.py
@@ -27,7 +27,7 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #
 ##############################################################################
-
+import res_currency
 import currency_rate_update
 import company
 import wizard
