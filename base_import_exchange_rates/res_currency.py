c49c47c208b0f7c88deb400c1c7f1db94d81dc2c
Ronald Rubi <ronald.rubi@clearcorp.co.cr>
2013-04-03 10:09:13 -0600

    [FIX] Fix base_import_exchange_rates module

--- base_import_exchange_rates/res_currency.py
+++ base_import_exchange_rates/res_currency.py
@@ -1,24 +1,21 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
 #
-#    __terp__.py
-#    base_import_exchange_rates
+#    base_currency_symbol.py
+#    base_currency_symbol
 #    First author: Carlos Vásquez <carlos.vasquez@clearcorp.co.cr> (ClearCorp S.A.)
 #    Copyright (c) 2010-TODAY ClearCorp S.A. (http://clearcorp.co.cr). All rights reserved.
-#    Inspired directly from Camp2Camp module c2c_currency_rate_update
-#    The module was copied and modified in order to provide enough flexibility to extend it
-#    to use as a generic WebService XML data importer into OpenERP.
-#    
+#
 #    Redistribution and use in source and binary forms, with or without modification, are
 #    permitted provided that the following conditions are met:
-#    
+#
 #       1. Redistributions of source code must retain the above copyright notice, this list of
 #          conditions and the following disclaimer.
-#    
+#
 #       2. Redistributions in binary form must reproduce the above copyright notice, this list
 #          of conditions and the following disclaimer in the documentation and/or other materials
 #          provided with the distribution.
-#    
+#
 #    THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ``AS IS'' AND ANY EXPRESS OR IMPLIED
 #    WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
 #    FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> OR
@@ -28,41 +25,20 @@
 #    ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
 #    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 #    ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#    
+#
 #    The views and conclusions contained in the software and documentation are those of the
 #    authors and should not be interpreted as representing official policies, either expressed
 #    or implied, of ClearCorp S.A..
-#    
+#
 ##############################################################################
 
-{
-    'name': 'Import Exchange Rates from XML',
-    'version': '0.1',
-    'url': 'http://launchpad.net/openerp-ccorp-addons',
-    'author': 'ClearCorp S.A.\nOriginally: Camptocamp S.A.\nContributor: Grzegorz Grzelak',
-    'website': 'http://clearcorp.co.cr',
-    'category': 'Generic Modules/Base',
-    'description': """Import exchange rates from XML web service.
-        Configuration:
-        1) Install the module.
-        2) Go to Administration > Company, open the company you want to syncronize.
-        3) in the page ´Currency auto update configuration´ you can configure wich webservice you wan to use
-           the company the currency is related to, and the currecy.
-        4) You have to go to Administration>Configuration>Planification , and configure the interval you want for the method to syncronize.
-         
-    
-    
-    
-    """,
-    'depends': ['account'],
-    'init_xml': ['security/security.xml'],
-    'demo_xml': [],
-    'update_xml': [
-        'currency_rate_update.xml',
-        'company_view.xml',
-        'res_currency_view.xml',
-        ],
-    'license': 'Other OSI approved licence',
-    'installable': True,
-    'active': False,
-}
+from osv import osv,fields
+
+class ResCurrency(osv.osv):
+
+    _inherit = 'res.currency'
+
+    _columns = {
+    #True if need to change the rate= 1/rate
+        'import_inverted': fields.boolean('Import Inverted'),
+    }
