468ae3adb70a1f4802e24f71f3c79505b2eeec64
Diana Rodriguez <diana.rodriguez@clearcorp.co.cr>
2013-04-24 10:34:28 -0600

    [FIX] Pass the base_currency_symbol module to openerp v7.0 [ADD] Add the tools_amount_to_text to account_report_lib module [ADD] Add webkit header check print bcr to report_webkit_lib module

--- base_currency_symbol/__openerp__.py
+++ base_currency_symbol/__openerp__.py
@@ -20,5 +20,24 @@
 #
 ##############################################################################
 
-import tools_modules_extended
-import tools_amount_to_text
+{
+	'name': 'Base currency symbol',
+	'version': '1.0',
+	'url': 'http://launchpad.net/openerp-ccorp-addons',
+	'author': 'ClearCorp S.A.',
+	'website': 'http://clearcorp.co.cr',
+	'category': 'General Modules/Base',
+	'description': """Adds symbol to currency:
+					  Use symbol_prefix and symbol_suffix depending on the currency standard.
+				   """,
+	'depends': ['base'],
+	'init_xml': [],
+	'demo_xml': [],
+	'update_xml': [
+		'base_currency_symbol_data.xml',
+		'base_currency_symbol_view.xml',
+		],
+	'license': 'Other OSI approved licence',
+	'installable': True,
+	'active': False,
+}
