c4cb2426b9d888734b1ade5b6035438ee9c56e42
Ronald Rubi <ronald.rubi@clearcorp.co.cr>
2013-07-30 14:55:40 -0600

    [FIX] Fix invalid import  in module base_import_exchange_rates

--- base_import_exchange_rates/__init__.py
+++ base_import_exchange_rates/__init__.py
@@ -27,7 +27,6 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #
 ##############################################################################
-import res_currency
 import currency_rate_update
 import company
 import wizard
