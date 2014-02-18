ca6c3c12abe6dcb7d93ca2b39a9f93db3ffcb606
Diana Rodriguez <diana.rodriguez@clearcorp.co.cr>
2014-02-18 16:49:55 -0600

    [FIX] Fix security problems with cash_flow_report, account_general_ledger_report and account_journal_period_report [FIX] Fix some issues asociated to translations

--- cash_flow_report/__openerp__.py
+++ cash_flow_report/__openerp__.py
@@ -26,15 +26,16 @@
     'category': 'Accounting & Finance',
     "description": """
 Cash Flow Report:
-=================
+===================
 Cash Flow report in Webkit. This report allows you to monitor company activities that move cash flow """,
     "website" : "http://clearcorp.co.cr",
     "depends" : ['account_report_lib', 'account_account_extended_ccorp',],
-    "data" : [
-                'report/report.xml',
+    "data" : [              
+                'security/ir.model.access.csv',  
                 'wizard/cash_flow_report_wizard.xml',
-                'cash_flow_type.xml',
+                'report/report.xml',
                 'report_menus.xml',
+                'cash_flow_type.xml',          
              ],
     'active': False,
     'installable': True,    
