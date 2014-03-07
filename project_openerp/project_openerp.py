83aa51f04ace0cebfa6d9ee6e4cac28f6628615c
Glen Sojo <glen.sojo@clearcorp.co.cr>
2014-03-07 08:35:21 -0600

    [ADD] - project_openerp: added work type to features

--- project_openerp/project_openerp.py
+++ project_openerp/project_openerp.py
@@ -22,9 +22,9 @@
 
 from openerp.osv import fields, osv
 
-class TaskType(osv.Model):
+class WorkType(osv.Model):
     
-    _name = 'project.oerp.task.type'
+    _name = 'project.oerp.work.type'
     
     _order = 'sequence'
     
@@ -43,5 +43,81 @@ class Phase(osv.Model):
     _inherit = 'project.phase'
     
     _columns = {
-                'task_type_ids': fields.one2many('project.oerp.task.type', 'phase_id', string='Task Types'),
+                'work_type_ids': fields.one2many('project.oerp.work.type', 'phase_id', string='Work Types'),
                 }
+    
+class FeatureHours(osv.Model):
+    
+    _name = 'project.oerp.feature.hours'
+    
+    #TODO: calculate hours from tasks with this work type
+    def _effective_hours(self, cr ,uid, ids, field_name, arg, context=None):
+        res = {}
+        for hour in self.browse(cr, uid, ids, context=context):
+            task_obj = self.pool.get('project.task')
+            #task_ids = task_obj.search(cr, uid, ids, [('feature_id','=', hour.feature_id)], context=context)
+            
+            res[hour.id] = 0.0
+        return res
+    
+    def _remaining_hours(self, cr ,uid, ids, field_name, arg, context=None):
+        res = {}
+        for hour in self.browse(cr, uid, ids, context=context):
+            res[hour.id] = hour.expected_hours - hour.effective_hours
+        return res
+    
+    _columns = {
+                'feature_id': fields.many2one('project.scrum.feature', string='Feature', required=True),
+                'project_id': fields.related('feature_id', 'product_backlog_id', 'project_id',
+                    type='many2one', relation='project.project', string='Project'),
+                'phase_id': fields.many2one('project.phase', string="Phase", 
+                    domain="[('project_id','=',project_id)]"),
+                'work_type_id': fields.many2one('project.oerp.work.type', string='Work Type',
+                    required=True, domain="[('phase_id','=',phase_id)]"),
+                'expected_hours': fields.float('Planned Hour(s)', required=True),
+                'effective_hours': fields.function(_effective_hours, type='float', string='Spent Hour(s)'),
+                'remaining_hours': fields.function(_remaining_hours, type='float', string='Remaining Hour(s)'),
+                }
+    
+    _defaults = {
+                 'feature_id': lambda slf, cr, uid, ctx: ctx.get('feature_id', False),
+                 'project_id': lambda slf, cr, uid, ctx: ctx.get('project_id', False),
+                 }
+    
+class Feature(osv.Model):
+    
+    _inherit = 'project.scrum.feature'
+    
+    _columns = {
+                'hour_ids': fields.one2many('project.oerp.feature.hours', 'feature_id', string='Feature Hours'),
+                }
+    
+    def write(self, cr, uid, ids, values, context=None):
+        if 'hour_ids' in values:
+            hours = values['hour_ids']
+            sum = 0.0
+            for hour in hours:
+                id = hour[1]
+                vals = hour[2]
+                if vals: 
+                    if 'expected_hours' in vals:
+                        sum += vals['expected_hours']
+                else:
+                    hour_obj = self.pool.get('project.oerp.feature.hours')
+                    sum += hour_obj.browse(cr, uid, id, context=context).expected_hours
+            values['expected_hours'] = sum
+        return super(Feature,self).write(cr, uid, ids, values, context=context)
+    
+class TaskWork(osv.Model):
+    
+    _inherit = 'project.task.work'
+    
+    _columns = {
+                'phase_id': fields.many2one('project.phase', string='Phase'),
+                'work_type_id': fields.many2one('project.oerp.work.type', string='Work Type',
+                    domain="[('phase_id','=',phase_id)]"),
+                }
+    
+    _defaults = {
+                 'phase_id': lambda slf, cr, uid, ctx: ctx.get('phase_id',False),
+                 }
