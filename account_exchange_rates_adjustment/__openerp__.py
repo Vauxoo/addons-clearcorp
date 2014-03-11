6f4e15f2467e0ae7d461cd23afa636a7e83cba8c
Diana Rodriguez <diana.rodriguez@clearcorp.co.cr>
2014-03-11 10:38:09 -0600

    [ADD] Add account_exchange_rates_adjustment module. This module add a rate adjustment to account depending of currency. [ADD] Add currency_rate_update_BCCR. This module provide conecction with BCCR to update rates for sale and purchase. Also create a webservice and for each currency, create a cronjob. This cronjob permits update each currency separaletly

---
+++ account_exchange_rates_adjustment/__openerp__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Addons modules by CLEARCORP S.A.
+#    Copyright (C) 2009-TODAY CLEARCORP S.A. (<http://clearcorp.co.cr>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    "name" : 'Account Exchange Rates Adjustment',
+    "version" : '1.0',
+    "author" : 'CLEARCORP S.A',
+    "category": 'Accounting & Finance',
+    "description": """
+Account Exchange Rates Adjustment:
+====================================
+    Adjust the amount of currency, 
+    at the exchange rate on a specified day
+    """,    
+    "depends" : [
+                 "account", 
+                 "currency_rate_update_BCCR", 
+                 "res_currency_sequence",
+                ],
+
+    "data" : [
+                "wizard/account_exchange_rates_adjustment.xml",
+                "account_exchange_rates_adjustment.xml",                 
+                ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'AGPL-3',
+}
