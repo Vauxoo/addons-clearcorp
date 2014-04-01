b4ac0e4709817bdcdc25f9144e9de8be7349bf61
Glen Sojo <glen.sojo@clearcorp.co.cr>
2014-04-01 11:57:27 -0600

    [FIX] - account_bank_statement_reconcile_ccorp: Fixed wrong license on py files

--- account_bank_statement_reconcile_ccorp/__init__.py
+++ account_bank_statement_reconcile_ccorp/__init__.py
@@ -1,14 +1,14 @@
-#-*- coding:utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>). All Rights Reserved
-#    d$
+#    Addons modules by CLEARCORP S.A.
+#    Copyright (C) 2009-TODAY CLEARCORP S.A. (<http://clearcorp.co.cr>).
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
