998e63a8193316a19ad7938c81f2e11fe67140b0
Glen Sojo <glen.sojo@clearcorp.co.cr>
2014-11-26 10:28:15 -0600

    [REF] - account_withholding_tax_comissions: Modified move_line_creation.

--- account_withholding_tax_comissions/account_voucher.py
+++ account_withholding_tax_comissions/account_voucher.py
@@ -60,51 +60,55 @@ class Voucher(osv.Model):
             line_ids, tax_id, partner_id, date, amount, ttype, company_id, context)
         if not vals:
             vals = {}
-
         if journal_id:
             journal_obj = self.pool.get('account.journal').browse(
                 cr, uid, journal_id, context)
-            # Get the withholding taxes
-            withholding_tax_lines = []
-            for tax in journal_obj.withholding_tax_ids:
-                amount_to_pay = 0.0
-                if tax.type == 'percentage':
-                    amount_to_pay += (amount * tax.amount) / 100.0
+            # Check if the journal has withholding taxes
+            if journal_obj.withholding_tax_ids:
+                if 'warning' in vals:
+                    vals['warning'] = {
+                        'title': 'Warning',
+                        'message': 'The journal selected has withholding taxes assigned. '
+                    'Please compute the taxes before validating the voucher.'
+                    }
                 else:
-                    amount_to_pay += tax.amount
-                withholding_tax_lines.append((0,0,{
-                    'amount': amount_to_pay,
-                    'withholding_tax_id': tax.id,
-                }))
-            if 'value' in vals:
-                vals['value'].update({
-                    'withholding_tax_lines': withholding_tax_lines
-                })
-            else:
-                vals['value'] = {}
-                vals['value'].update({
-                    'withholding_tax_lines': withholding_tax_lines,
-                })
-        else:
-            if 'value' in vals:
-                vals['value'].update({
-                    'withholding_tax_lines': [(5,0,{}),],
-                })
-            else:
-                vals['value'] = {}
-                vals['value'].update({
-                    'withholding_tax_lines': [(5,0,{}),],
-                })
+                    vals['warning'] = {
+                        'title': 'Warning',
+                        'message': 'The journal selected has withholding taxes assigned. '
+                    'Please compute the taxes before validating the voucher.'
+                    }
         return vals
 
     def _check_duplicate_withholding_taxes(self, cr, uid, ids, context=None):
         for voucher in self.browse(cr, uid, ids, context=context):
             tax_ids = []
             for tax in voucher.withholding_tax_lines:
-                if tax.id not in tax_ids:
-                    tax_ids.append(tax.id)
+                if tax.withholding_tax_id.id not in tax_ids:
+                    tax_ids.append(tax.withholding_tax_id.id)
                 else:
                     return False
+        return True
+
+    def compute_withholding_taxes(self, cr, uid, ids, context=None):
+        for voucher in self.browse(cr, uid, ids, context=context):
+            withholding_tax_lines = []
+            for tax in voucher.journal_id.withholding_tax_ids:
+                amount_to_pay = 0.0
+                if tax.type == 'percentage':
+                    amount_to_pay += (voucher.amount * tax.amount) / 100.0
+                else:
+                    amount_to_pay += tax.amount
+                withholding_tax_lines.append((0,0,{
+                    'amount': amount_to_pay,
+                    'withholding_tax_id': tax.id,
+                }))
+            tax_ids = [tax.id for tax in voucher.withholding_tax_lines]
+            self.pool.get('account.voucher.withholding.line').unlink(
+                cr, uid, tax_ids, context=context)
+            if withholding_tax_lines:
+                voucher.write({
+                    'withholding_tax_lines': withholding_tax_lines
+                })
         return True
 
     _columns = {
@@ -130,83 +134,65 @@ class Voucher(osv.Model):
 
     _constraints = [
         (_check_duplicate_withholding_taxes,'Withholding taxes can not be duplicated.',
-         ['voucher_withholding_tax_optional']),
+         ['withholding_tax_lines']),
     ]
 
     #Method that create the move and move lines from withholding tax
     def action_move_line_create(self, cr, uid, ids, context=None):
-
-        #1. Super of action_move_line_create
         res = super(Voucher, self).action_move_line_create(cr, uid, ids, context)
-
+        move_obj = self.pool.get('account.move')
+        currency_obj = self.pool.get('res.currency')
         for voucher in self.browse(cr, uid, ids, context=context):
             # Currency of voucher and company
             company_currency = self._get_company_currency(cr, uid, voucher.id, context)
             current_currency = self._get_current_currency(cr, uid, voucher.id, context)
 
-            # For each withholding tax, keep amount or percentage.
-           
-            #*****************REQUIRED WITHHOLDING TAX
-            # For required withholding tax, use voucher_withholding_tax_required_inv field
-            for withholding in voucher.withholding_tax_required:
-                
-                #********************MOVE
-                #Create move_id (the method account_move_get return a dictionary)
-                
-                move_id_name = voucher.move_id.ref + ' ' + withholding.code 
+            for tax in voucher.withholding_tax_lines:
+                # Create the withholding move
+                move_name = voucher.move_id.ref + ' ' + tax.withholding_tax_id.code 
                 move_dict = self.account_move_get(cr, uid, voucher.id, context=context)
-                
                 move_dict['name'] = '/'
-                move_dict['journal_id'] = withholding.journal_id.id
-                move_dict['ref'] = move_id_name
-                move_dict['narration'] = move_id_name
-                
-                #Associate voucher with move
+                move_dict['journal_id'] = tax.withholding_tax_id.journal_id.id
+                move_dict['ref'] = move_name
+                move_dict['narration'] = move_name
                 move_dict['withholding_voucher_id'] =  voucher.id
-                
-                move_id = self.pool.get('account.move').create(cr, uid, move_dict, context=context)  
-                   
-                ##*******************AMOUNTS
+                move_id = move_obj.create(cr, uid, move_dict, context=context)
+
+                amount_currency = 0.0
+                amount = 0.0
+                """ Code found in account_voucher.py check if it is necessary to compute a 
+                special currency
+                date = self.read(cr, uid, voucher_id, ['date'], context=context)['date']
+                ctx = context.copy()
+                ctx.update({'date': date})
+                voucher = self.pool.get('account.voucher').browse(cr, uid, voucher_id, context=ctx)
+                voucher_currency = voucher.journal_id.currency or voucher.company_id.currency_id
+                ctx.update({
+                    'voucher_special_currency_rate': voucher_currency.rate * voucher.payment_rate ,
+                    'voucher_special_currency': voucher.payment_rate_currency_id and voucher.payment_rate_currency_id.id or False,})"""
                 if company_currency <> current_currency:
-                    if withholding.type == 'percentage':
-                        withholding_amount = voucher.amount * (withholding.amount / 100)
-                        amount_currency = withholding_amount
-                    else:
-                        withholding_amount = withholding.amount
-                        amount_currency = (withholding.amount * voucher.paid_amount_in_company_currency) / voucher.amount                    
+                    amount = currency_obj.compute(cr, uid, current_currency,
+                        company_currency, tax.amount, context=context)
+                    amount_currency = tax.amount
                 else:
-                    #For each withholding tax, create two move lines
-                    #Get amount or percentage associate to withholding tax
-                    if withholding.type == 'percentage':
-                        withholding_amount = voucher.paid_amount_in_company_currency * (withholding.amount / 100)
-                    else:
-                        withholding_amount = withholding.amount
-                    
-                    amount_currency = 0.0
-                #**************************
-                
-                #********************ACCOUNTS
-                #Get account to associate the move line and debit or credit
+                    amount = tax.amount
+                # Get accounts based on the voucher type
                 if voucher.type in ('purchase', 'payment'):
                     account_debit = voucher.account_id
-                    account_credit = withholding.journal_id.default_credit_account_id
-                
+                    account_credit = tax.withholding_tax_id.journal_id.default_credit_account_id
                 elif voucher.type in ('sale', 'receipt'):
-                    account_debit = withholding.journal_id.default_debit_account_id
+                    account_debit = tax.withholding_tax_id.journal_id.default_debit_account_id
                     account_credit = voucher.account_id
-                #****************************
-                
-                #Line name
-                line_name = voucher.move_id.ref + ' ' + withholding.code
-                        
-                #create move_lines (2 for each withhlding):
+
+                line_name = voucher.move_id.ref + ' ' + tax.withholding_tax_id.code
+                # Create the move lines for each tax
                 move_line_debit = {
                     'name': line_name,
-                    'debit': withholding_amount,
+                    'debit': amount,
                     'credit': 0.0,
                     'account_id': account_debit.id,
                     'move_id': move_id,
-                    'journal_id': withholding.journal_id.id,
+                    'journal_id': tax.withholding_tax_id.journal_id.id,
                     'period_id': voucher.period_id.id,
                     'partner_id': voucher.partner_id.id,
                     'currency_id': company_currency <> current_currency and  current_currency or False,
@@ -218,134 +204,25 @@ class Voucher(osv.Model):
                 move_line_credit = {
                     'name': line_name,
                     'debit': 0.0,
-                    'credit': withholding_amount,
+                    'credit': amount,
                     'account_id': account_credit.id,
                     'move_id': move_id,
-                    'journal_id': withholding.journal_id.id,
+                    'journal_id': tax.withholding_tax_id.journal_id.id,
                     'period_id': voucher.period_id.id,
                     'partner_id': voucher.partner_id.id,
                     'currency_id': company_currency <> current_currency and  current_currency or False,
-                    'amount_currency': amount_currency,
-                    'currency_id': 0.0,
-                    'amount_currency':0.0,
+                    'amount_currency': -1.0 * amount_currency,
                     'date': voucher.date,
                     'date_maturity': voucher.date_due
                 }
-
-                #Create move_lines
                 self.pool.get('account.move.line').create(cr, uid, move_line_credit, context)
-                
                 self.pool.get('account.move.line').create(cr, uid, move_line_debit, context)
-                
-                
-            #*****************OPTIONAL WITHHOLDING TAX
-            # For required withholding tax, use voucher_withholding_tax_required_inv field
-            for withholding in voucher.withholding_tax_optional:
-                
-                #********************MOVE
-                #Create move_id (the method account_move_get return a dictionary)
-                
-                move_id_name = voucher.move_id.ref + ' ' + withholding.code 
-                move_dict = self.account_move_get(cr, uid, voucher.id, context=context)
-                
-                move_dict['name'] = '/'
-                move_dict['journal_id'] = withholding.journal_id.id
-                move_dict['ref'] = move_id_name
-                move_dict['narration'] = move_id_name
-                
-                #Associate voucher with move
-                move_dict['withholding_voucher_id'] =  voucher.id
-                
-                move_id = self.pool.get('account.move').create(cr, uid, move_dict, context=context)
-                   
-                ##*******************AMOUNTS
-                #Check if voucher currency is diferent that currency company
-                if company_currency <> current_currency:
-                    if withholding.type == 'percentage':
-                        withholding_amount = voucher.amount * (withholding.amount / 100)
-                        amount_currency = withholding_amount
-                    else:
-                        withholding_amount = withholding.amount
-                        amount_currency = (withholding.amount * voucher.paid_amount_in_company_currency) / voucher.amount
-                    
-                else:
-                    if withholding.type == 'percentage':
-                       withholding_amount = voucher.paid_amount_in_company_currency * (withholding.amount / 100)
-                       
-                    else:
-                        withholding_amount = withholding.amount
-                    
-                    amount_currency = 0.0
-                        
-                #**************************
-                
-                #********************ACCOUNTS
-                #Get account to associate the move line and debit or credit
-                if voucher.type in ('purchase', 'payment'):
-                    account_debit = voucher.account_id
-                    account_credit = withholding.journal_id.default_credit_account_id
-                
-                elif voucher.type in ('sale', 'receipt'):
-                    account_debit = withholding.journal_id.default_debit_account_id
-                    account_credit = voucher.account_id
-            
-                #****************************
-                
-                #Line name
-                line_name = voucher.move_id.ref + ' ' + withholding.code
-                        
-                #create move_lines (2 for each withhlding):
-                move_line_debit = {
-                    'name': line_name,
-                    'debit': withholding_amount,
-                    'credit': 0.0,
-                    'account_id': account_debit.id,
-                    'move_id': move_id,
-                    'journal_id': withholding.journal_id.id,
-                    'period_id': voucher.period_id.id,
-                    'partner_id': voucher.partner_id.id,
-                    'currency_id': company_currency <> current_currency and  current_currency or False,
-                    'amount_currency': amount_currency,
-                    'date': voucher.date,
-                    'date_maturity': voucher.date_due
-                }
-                
-                move_line_credit = {
-                    'name': line_name,
-                    'debit': 0.0,
-                    'credit': withholding_amount,
-                    'account_id': account_credit.id,
-                    'move_id': move_id,
-                    'journal_id': withholding.journal_id.id,
-                    'period_id': voucher.period_id.id,
-                    'partner_id': voucher.partner_id.id,
-                    'currency_id': company_currency <> current_currency and  current_currency or False,
-                    'amount_currency': amount_currency,
-                    'currency_id': 0.0,
-                    'amount_currency':0.0,
-                    'date': voucher.date,
-                    'date_maturity': voucher.date_due
-                }
-                
-                #Create move_lines
-                self.pool.get('account.move.line').create(cr, uid, move_line_credit, context)
-                
-                self.pool.get('account.move.line').create(cr, uid, move_line_debit, context)
+        return res
 
-        return True
-
-    #When the voucher reverse, the account move for withholding tax reverse too.
     def reverse_voucher(self, cr, uid, ids, context=None):
-        
-        reverse_withholing_list = []
-        
-        #Reverse "normal" voucher.
         res = super(Voucher, self).reverse_voucher(cr, uid, ids, context)
         
-        #Reverse "withholding voucher"
         for voucher in self.browse(cr, uid, ids, context=context):
             for withholding_move in voucher.withholding_move_ids:
                 self.pool.get('account.move').reverse(cr, uid, [withholding_move.id], context=context)
-            self.write(cr, uid, [voucher.id], {'state' : 'reverse'}, context=context)
-
         return res
