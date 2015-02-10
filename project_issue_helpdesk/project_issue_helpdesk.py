6faac546106dbd992b34c3155156942bf0c66280
Carlos Navarro <carlos.navarro@imesasistemas.com>
2015-02-10 15:15:50 -0600

    [REM]Remove the pricelist contract and holidays calendar

--- project_issue_helpdesk/project_issue_helpdesk.py
+++ project_issue_helpdesk/project_issue_helpdesk.py
@@ -215,7 +215,7 @@ class HrAnaliticTimeSheet(osv.Model):
                 'start_time': fields.float(required=True,string="Start Time"),
                 'end_time': fields.float(required=True,string="End Time"),
                 'service_type': fields.selection([('expert','Expert'),('assistant','Assistant')],required=True,string="Service Type"),                       
-                'unit_amount':fields.function(_compute_duration, type='float', string='Quantify',store=True),
+                #'unit_amount_temp':fields.function(_compute_duration, type='float', string='Quantify',store=True),
                 'employee_id': fields.many2one('hr.employee', 'Technical Staff'),
                 }
      
@@ -233,120 +233,6 @@ class StockPicking(orm.Model):
      _columns = {
           'issue_id': fields.many2one('project.issue')
      }
- 
-class ResPartner(orm.Model):
-    _inherit = 'res.partner'
-     
-    def onchange_partner_type(self, cr, uid, ids,partner_type,context={}):
-        res={}
-         
-        if partner_type=='company':
-            res['is_company'] = True
-        elif partner_type=='branch':
-            res['is_company'] = True
-        elif partner_type=='contact':
-            res['is_company'] = False
-        return {'value': res}
-     
-    _columns = {
-        'partner_type': fields.selection([('company','Company'),('branch','Branch'),('contact','Contact')],required=True,string="Partner Type"),
-        'provision_amount':fields.float(digits=(16,2),string="Provision Amount")
-     }
-    _defaults={
-        'provision_amount':0.0
-        }
-    
-class ContractPricelist(orm.Model):
-    _name = 'contract.pricelist'
- 
-    _columns = {
-        'name':fields.char(size=256,required=True,string="Name"),
-        'account_analytic_id':fields.many2one('account.analytic.account',required=True,string="Account Analytic"),
-        'partner_id':fields.related('account_analytic_id','partner_id',relation='res.partner',type='many2one',string="Partner"),
-        'calendar_id':fields.many2one('resource.calendar',required=True,string="Calendar"),
-        'line_ids': fields.one2many('contract.pricelist.line','contract_pricelist_id')
-        }
-    _sql_constraints = [
-        ('account_analytic_unique',
-        'UNIQUE(account_analytic_id)',
-        'Account Analytic already exist ')
-                        ]
-     
-class ContractPriceLine(orm.Model):
-    _name = 'contract.pricelist.line'
- 
-    def onchange_pricelist_line_type(self, cr, uid, ids,pricelist_line_type,context={}):
-        res={}
-         
-        if pricelist_line_type:
-            if pricelist_line_type=='category':
-                res['product_id'] = ""
-            elif pricelist_line_type=='product':
-                res['categ_id'] = ""
-        elif not pricelist_line_type:
-             res['product_id'] = ""
-             res['categ_id'] = ""
-        return {'value': res}
-            
-    def _check_lines(self, cr, uid, ids, context={}):
-        if context is None:   
-            context = {}
-        for lines in self.browse(cr, uid, ids, context=context):
-            if lines.pricelist_line_type=='category':
-                exist_category = self.search(cr, uid, [('categ_id','=', lines.categ_id.id),('contract_pricelist_id','=', lines.contract_pricelist_id.id)])
-                if len(exist_category)>1:
-                    return False
-            elif lines.pricelist_line_type=='product':
-                exist_product = self.search(cr, uid, [('product_id','=', lines.product_id.id),('contract_pricelist_id','=', lines.contract_pricelist_id.id)])
-                if len(exist_product)>1:
-                    return False
-        return True
- 
-    def _check_rates(self, cr, uid, ids, context={}):
-        for rates in self.browse(cr, uid, ids, context=context):
-            if (rates.technical_rate<1.0 or rates.assistant_rate<1.0):
-                return False
-        return True
-     
-    def _check_multipliers(self, cr, uid, ids, context={}):
-        for multipliers in self.browse(cr, uid, ids, context=context):
-            if (multipliers.overtime_multiplier<1.0 or multipliers.holiday_multiplier<1.0):
-                return False
-        return True
-     
-    _columns = {
-        'contract_pricelist_id':fields.many2one('contract.pricelist',string="Contract Pricelist"),
-        'pricelist_line_type': fields.selection([('category','Category'),('product','Product')],string="Pricelist Type"),                                 
-        'categ_id':fields.many2one('product.category',string="Category Product"),
-        'product_id':fields.many2one('product.product',string="Product"),
-        'technical_rate':fields.float(digits=(16,2),required=True,string="Technical Rate"),
-        'assistant_rate':fields.float(digits=(16,2),required=True,string="Assistant Rate"),
-        'overtime_multiplier':fields.float(digits=(16,2),required=True,string="Overtime Multiplier"),
-        'holiday_multiplier':fields.float(digits=(16,2),required=True,string="Holiday Multiplier")
-       }
-     
-    _defaults={
-        'technical_rate':0.0,
-        'assistant_rate':0.0,
-        'overtime_multiplier':1.0
-        }
-              
-    _constraints = [
-        (_check_lines,'Contract only allow a line to a single product o category',['categ_id','product_id']),
-        (_check_rates,'Rates must be greater or equal to one',['technical_rate','assistant_rate']),
-        (_check_multipliers,'Multipliers must be greater or equal to one',['overtime_multiplier','holiday_multiplier']) 
-                ]
- 
- 
-class HolidayCalendar(orm.Model):
-    _name = 'holiday.calendar'
- 
-    _columns = {
-        'name':fields.char(size=256,required=True,string="Name"),
-        'date':fields.date(required=True,string="Date"),
-        'notes':fields.text(string="Notes")
-        }        
-
 class ProductTemplate(orm.Model):
     _inherit = 'product.template'
     _name = 'product.template'
