17cd95a219689c630a28cd7e3711a52659899319
Carlos Navarro <carlos.navarro@imesasistemas.com>
2015-03-31 12:26:06 -0600

    [ADD] Add worklogs to task project

---
+++ project_task_helpdesk/__openerp__.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Addons modules by CLEARCORP S.A.
+#    Copyright (C) 2009-TODAY CLEARCORP S.A. (<http://clearcorp.co.cr>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Project Task Helpdesk',
+    'version': '1.0',
+    'author' : 'ClearCorp',
+    'complexity': 'normal',
+    'website': 'http://clearcorp.co.cr',
+    'category': 'Project',
+    'description': """
+Extends Project Task
+===============================================
+
+""",
+    'depends': ['project_issue_helpdesk'],
+    'init_xml': [],
+    'demo_xml': [],
+    'data': ['view/project_task_helpdesk.xml',
+             #'view/project_issue_helpdesk_menu.xml',
+             #'data/project_issue_sequence_data.xml'
+             ],
+    'license': 'AGPL-3',
+    'installable': True,
+    'active': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
+
---
+++ project_task_helpdesk/project_task_helpdesk.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Addons modules by CLEARCORP S.A.
+#    Copyright (C) 2009-TODAY CLEARCORP S.A. (<http://clearcorp.co.cr>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp import models, fields, api, _
+from openerp.exceptions import Warning
+
+class ProjectTask(models.Model):
+    _inherit = 'project.task'
+    @api.depends('project_id')
+    @api.one
+    def get_account_id(self):
+        if self.project_id:
+            if self.project_id.analytic_account_id:
+                self.analytic_account_id=self.project_id.analytic_account_id
+            else:
+                self.analytic_account_id=False
+
+    timesheet_ids=fields.One2many('hr.analytic.timesheet','task_id')
+    analytic_account_id = fields.Many2one('account.analytic.account',compute="get_account_id",string='Analytic Account',store=True)
+    is_closed = fields.Boolean(string='Is Closed',related='stage_id.closed',store=True)
