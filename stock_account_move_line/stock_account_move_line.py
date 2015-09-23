4ce8f10751801c08a8cbf99d6797d024f758cf22
fv-clearcorp <fabio.vilchez@clearcorp.co.cr>
2015-09-23 12:08:01 -0600

    [FIX] stock_account_move_line: Fixed error for fields unit cost and unit cost currency.

--- stock_account_move_line/stock_account_move_line.py
+++ stock_account_move_line/stock_account_move_line.py
@@ -28,31 +28,32 @@ class account_move_line(models.Model):
 
     _inherit = "account.move.line"
 
-    @api.one
-    def _compute_unit_cost(self):
+    def compute_unit_cost(self,cr, uid, quantity, debit, credit, context={}):
         cost = 0.0
-        if self.quantity != 0.0:
-            cost = self.debit + self.credit / self.quantity
-            self.unit_cost = cost
-    @api.one
-    def _compute_unit_cost_currency(self):
+        if quantity != 0.0:
+            cost = (debit + credit) / quantity
+        return cost
+
+    def compute_unit_cost_currency(self, cr, uid, stock_picking, unit_cost, context = {}):
         currency = False
-        if self.stock_picking_id.sale_id:
-            currency = self.stock_picking_id.sale_id.currency_id or False
+        if stock_picking.sale_id:
+            currency = stock_picking.sale_id.currency_id or False
         else:
-            for move in self.stock_picking_id.move_lines:
+            for move in stock_picking.move_lines:
                 if move.purchase_line_id.order_id:
                     currency = move.purchase_line_id.order_id.currency_id or False
                     break
-        if not currency or currency == self.company_id.currency_id:
-            self.unit_cost_currency = 0.0
+        if not currency or currency == stock_picking.company_id.currency_id:
+            unit_cost_currency = 0.0
         else:
-            self.unit_cost_currency = currency.compute(self.unit_cost,
-                                                       self.company_id.currency_id)
+            company_currency = stock_picking.company_id.currency_id
+            unit_cost_currency = self.pool.get('res.currency').compute(cr, uid,
+                                            company_currency.id, currency.id, unit_cost,
+                                            context=context)
+        return unit_cost_currency
 
-    unit_cost = fields.Float('Unit Cost', compute='_compute_unit_cost', store=True)
-    unit_cost_currency = fields.Float('Unit Cost Currency',
-                                      compute='_compute_unit_cost_currency', store=True)
+    unit_cost = fields.Float('Unit Cost')
+    unit_cost_currency = fields.Float('Unit Cost Currency')
     stock_move_id = fields.Many2one('stock.move', string='Stock Move')
     stock_picking_id = fields.Many2one('stock.picking', string='Stock Picking')
 
@@ -65,11 +66,25 @@ class stock_quant(models.Model):
         res = super(stock_quant, self)._prepare_account_move_line(cr, uid, move, qty, cost, credit_account_id, debit_account_id, context=context)
         debit_line_vals = res[0][2]
         credit_line_vals = res[1][2]
+        move_line_obj = self.pool.get('account.move.line')
+        unit_cost_debit = move_line_obj.compute_unit_cost(cr, uid, quantity = debit_line_vals['quantity'],
+                                                                            debit = debit_line_vals['debit'],
+                                                                            credit = debit_line_vals['credit'],
+                                                                            context = context)
+        unit_cost_credit = move_line_obj.compute_unit_cost(cr, uid, quantity = credit_line_vals['quantity'],
+                                                                            debit = credit_line_vals['debit'],
+                                                                            credit = credit_line_vals['credit'],
+                                                                            context = context)
         debit_line_vals.update({'stock_move_id': move.id,
                                 'stock_picking_id': move.picking_id.id,
+                                'unit_cost': unit_cost_debit,
+                                'unit_cost_currency': move_line_obj.compute_unit_cost_currency(cr, uid, move.picking_id, unit_cost_debit, context=context),
+
                                 })
         credit_line_vals.update({'stock_move_id': move.id,
                                  'stock_picking_id': move.picking_id.id,
+                                 'unit_cost': unit_cost_credit,
+                                 'unit_cost_currency': move_line_obj.compute_unit_cost_currency(cr, uid, move.picking_id, unit_cost_credit, context=context),
                                  })
         res = [(0, 0, debit_line_vals), (0, 0, credit_line_vals)]
         return res
