b27b39500e6dd5861351403dca13d462c404ad19
fv-clearcorp <fabio.vilchez@clearcorp.co.cr>
2015-11-06 13:06:57 -0600

    [FIX] hr_payroll_extended: Fixed module added wizard for duplicated field period in payroll.

--- hr_payroll_extended/hr_payroll_extended.py
+++ hr_payroll_extended/hr_payroll_extended.py
@@ -230,22 +230,3 @@ class Payslip(osv.osv):
         if 'period_id' in context:
             vals.update({'period_id': context.get('period_id')})
         return super(Payslip, self).create(cr, uid, vals, context=context)
-
-
-class hr_payslip_employees(osv.osv_memory):
-
-    _inherit = 'hr.payslip.employees'
-
-    def compute_sheet(self, cr, uid, ids, context=None):
-        run_pool = self.pool.get('hr.payslip.run')
-        if context is None:
-            context = {}
-        if context.get('active_id'):
-            run_data = run_pool.read(cr, uid, context['active_id'],
-                                     ['period_id'])
-        period_id = run_data.get('period_id')
-        period_id = period_id and period_id[0] or False
-        if period_id:
-            context = dict(context, period_id=period_id)
-        return super(hr_payslip_employees, self).compute_sheet(cr, uid, ids,
-                                                               context=context)
