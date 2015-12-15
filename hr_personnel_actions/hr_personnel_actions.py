fd5ccc59568292181e597e797174f3066af5680e
Fabio Vilchez <fabio.vilchez@clearcorp.co.cr>
2015-12-15 16:30:26 -0600

    [FIX]- hr_personnel_actions: Fixed errors in context.

--- hr_personnel_actions/hr_personnel_actions.py
+++ hr_personnel_actions/hr_personnel_actions.py
@@ -104,7 +104,7 @@ class hrContract(osv.Model):
                 }
             action_obj = self.pool.get('hr.personnel.actions.personnel.action')
             action_obj.create(cr, uid, new_action, context=context)
-            action_obj.action_approved(cr, uid, ids, contex=context)
+            action_obj.action_approved(cr, uid, ids, context=context)
         else:
             raise osv.except_osv(_(
                 'Personnel Actions Error'),
