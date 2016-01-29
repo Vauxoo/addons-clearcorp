0c387cf22fce22c1a3968b55402e8c9771a301e9
Rodney Vargas <rodney.vargas@clearcorp.co.cr>
2016-01-29 14:46:34 -0600

    [FIX]-website_contact_form: change the string in generate_google_map_url method

--- website_contact_form/controllers/main.py
+++ website_contact_form/controllers/main.py
@@ -11,8 +11,8 @@ from openerp.http import request
 class contactus(http.Controller):
 
     def generate_google_map_url(self, street, city, city_zip, country_name):
-        url = "http://maps.googleapis.com/maps/api/staticmap?center=%s&"
-        "sensor=false&zoom=8&size=298x298" % werkzeug.url_quote_plus(
+        url = """http://maps.googleapis.com/maps/api/staticmap?center=%s
+&sensor=false&zoom=8&size=298x298""" % werkzeug.url_quote_plus(
             '%s, %s %s, %s' % (street, city, city_zip, country_name)
         )
         return url
