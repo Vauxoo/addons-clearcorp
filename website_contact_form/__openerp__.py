7114dc0757872bbe413b828f2b5f8deb5b5fddc5
Rodney Vargas <rodney.vargas@clearcorp.co.cr>
2016-02-12 09:00:56 -0600

    [ADD]-website_contact_form: add the snnipet contact form to features

--- website_contact_form/__openerp__.py
+++ website_contact_form/__openerp__.py
@@ -11,6 +11,10 @@ OpenERP Contact Form
         """,
     'author': 'ClearCorp',
     'depends': ['website_partner', 'crm', 'website_crm'],
+    'data': ['view.xml'],
+    'css': [
+            'static/src/css/style.css',
+            ],
     'installable': True,
     'auto_install': False,
 }
