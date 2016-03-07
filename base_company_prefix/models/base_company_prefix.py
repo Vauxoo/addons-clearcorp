7d69a70af4ebe5e59a6872addd889dd62708e3ab
Rodney Vargas <rodney.vargas@clearcorp.co.cr>
2016-03-07 16:16:31 -0600

    [ADD]-base_company_prefix: migrate from V8 to V9

---
+++ base_company_prefix/models/base_company_prefix.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# © 2016 ClearCorp
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp.osv import fields, orm
+
+
+class res_company(orm.Model):
+    _name = "res.company"
+    _inherit = 'res.company'
+    _order = 'prefix'
+
+    _columns = {
+        'prefix': fields.char('Prefix', size=10),
+    }
+
+    def name_get(self, cr, uid, ids, context=None):
+        if not len(ids):
+            return []
+        res = []
+        for obj_company in self.browse(cr, uid, ids, context=context):
+            obj_company_name = obj_company.prefix and obj_company.prefix + \
+                ' ' or ''
+            obj_company_name += obj_company.name
+            res.append((obj_company.id, obj_company_name))
+        return res
