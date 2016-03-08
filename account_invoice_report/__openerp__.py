a8a648a1a2d7463b19211debc97cf918801d9d8b
Rodney Vargas <rodney.vargas@clearcorp.co.cr>
2016-03-08 14:43:59 -0600

    [ADD]-account_invoice_report:update in base to the module template

--- account_invoice_report/__openerp__.py
+++ account_invoice_report/__openerp__.py
@@ -1,43 +1,23 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Addons modules by CLEARCORP S.A.
-#    Copyright (C) 2009-TODAY CLEARCORP S.A. (<http://clearcorp.co.cr>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
+# © 2016 ClearCorp
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name' : 'Invoice Report',
-    'version' : '1.0',
-    'author' : 'ClearCorp',
-    'complexity': 'normal',
-    'description': """
-            Invoice webkit report
-    """,
+    'name': 'Invoice Report',
+    'summary': 'Invoice webkit report',
+    'version': '8.0.1.0',
     'category': 'Accounting & Finance',
-    'sequence': 4,
-    'website' : 'http://clearcorp.co.cr',
-    'images' : [],
-    'depends' : [
+    'website': 'http://clearcorp.cr',
+    'author': 'ClearCorp',
+    'license': 'AGPL-3',
+    'sequence': 10,
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+    'depends': [
         'base',
         'account_invoice_discount',
-        'account_report_lib',
         ],
-    'data' : [
+    'data': [
         'data/report.paperformat.xml',
         'data/invoice_report.xml',
         'views/report_invoice_layout.xml',
@@ -45,9 +25,4 @@
         'views/report_invoice_layout_footer.xml',
         'views/report_account_invoice.xml',
     ],
-    'test' : [],
-    'auto_install': False,
-    'application': False,
-    'installable': True,
-    'license': 'AGPL-3',
 }
