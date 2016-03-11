975894299a5b5475790cc649baafe7febf24bb31
Glen Sojo <glen.sojo@clearcorp.co.cr>
2016-03-11 17:01:55 -0600

    [ADD] - purchase_order_report: Added contributors

--- purchase_order_report/__openerp__.py
+++ purchase_order_report/__openerp__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # © 2016 ClearCorp
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 {
     'name': 'Purchase Order Report',
     'summary': 'The purchase order request for quotation report',
