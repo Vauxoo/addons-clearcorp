2e6fbf564ca3dfa73224a61603196e11286fe72b
Rodney Vargas <rodney.vargas@clearcorp.co.cr>
2016-03-14 11:06:40 -0600

    [ADD]-sale_order_discount:change the methods and new fields in the sale order

--- sale_order_discount/models/sale_order_discount.py
+++ sale_order_discount/models/sale_order_discount.py
@@ -2,44 +2,69 @@
 # © 2016 ClearCorp
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import api, fields, models
+from openerp import models, fields, api
+import openerp.addons.decimal_precision as dp
 
 
-class SaleOrderLine(models.Model):
+class InvoiceLine(models.Model):
+    """Inherits account.invoice.line
+    and adds discount feature."""
+
     _inherit = 'sale.order.line'
-    _description = 'Sale Order Line'
 
-    discount = fields.Float('Discount (%)', digits=(16, 2))
+    @api.multi
+    @api.depends('price_unit', 'product_uom_qty')
+    def _compute_amount_line_no_discount(self):
+        for line in self:
+            if line.order_id and line.order_id.currency_id:
+                cur = line.order_id.currency_id
+                line.price_subtotal_not_discounted = cur.round(
+                    line.price_unit * line.product_uom_qty)
+            else:
+                line.price_subtotal_not_discounted = \
+                    line.price_unit * line.product_uom_qty
 
-    _defaults = {
-        'discount': lambda *a: 0.0,
-        }
-
-    _sql_constraints = [
-        ('check_discount', 'CHECK (discount < 100)',
-         'The line discount must be leaser than 100 !'),
-    ]
+    price_subtotal_not_discounted = fields.Float(
+        compute='_compute_amount_line_no_discount',
+        store=True, string='Subtotal')
 
 
 class SaleOrder(models.Model):
+    """Inherits account.invoice to
+    add global discount feature."""
+
     _inherit = 'sale.order'
-    _description = 'Sale Order'
 
-    @api.depends('order_line.price_total')
-    def _amount_all(self):
-        super(SaleOrder, self)._amount_all()
+    @api.multi
+    @api.depends('order_line.price_subtotal_not_discounted')
+    def _compute_amount_all(self):
         for order in self:
-            amount_discount = 0.0
-            for line in order.order_line:
-                amount_discount += ((line.discount / 100) *
-                                    line.price_subtotal)
-            amount_total = order.amount_untaxed + order.amount_tax - \
-                amount_discount
-            order.update({
-                'amount_discount': order.currency_id.round(
-                    amount_discount),
-                'amount_total': order.currency_id.round(amount_total),
-            })
+            amount_untaxed_not_discounted = 0.0
+            amount_discounted = 0.0
+            order_discount = 0.0
 
-    amount_discount = fields.Monetary(compute='_amount_all',
-                                      string='Discount', store=True)
+            for line in order.order_line:
+                amount_untaxed_not_discounted += \
+                    line.price_subtotal_not_discounted
+                amount_discounted += line.price_subtotal_not_discounted - \
+                    line.price_subtotal
+            if amount_untaxed_not_discounted:
+                order_discount = 100 * amount_discounted / \
+                    amount_untaxed_not_discounted
+            order.amount_untaxed_not_discounted = \
+                amount_untaxed_not_discounted
+            order.amount_discounted = amount_discounted
+            order.order_discount = order_discount
+
+    order_discount = fields.Float(
+        compute='_compute_amount_all',
+        digits=dp.get_precision('Account'),
+        string='Discount (%)')
+    amount_discounted = fields.Float(
+        compute='_compute_amount_all',
+        digits=dp.get_precision('Account'),
+        string='Discount', store=True)
+    amount_untaxed_not_discounted = fields.Float(
+        compute='_compute_amount_all',
+        digits=dp.get_precision('Account'),
+        string='Subtotal', store=True)
