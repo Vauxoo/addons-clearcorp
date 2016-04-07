24c62804d7b1594ebc9e7d4463875e60b12e7846
Rodney <rodney.vargas@clearcorp.co.cr>
2016-04-07 12:35:13 -0600

    [MRG] - hr_payroll_multicurrency: add the module to do the payroll mult… (#591)
    * [ADD]-hr_payroll_multicurrency: add the module to do the payroll multi-currency
    
    * [FIX]-hr_payroll_multicurrency: change the method process_sheet

---
+++ hr_payroll_multicurrency/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# © 2016 ClearCorp
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'HR Payroll MultiCurrency',
+    'summary': 'Module summary',
+    'version': '8.0.1.0',
+    'category': 'HR',
+    'website': 'http://clearcorp.cr',
+    'author': 'ClearCorp',
+    'license': 'AGPL-3',
+    'sequence': 10,
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+    'depends': [
+        'hr_payroll',
+    ],
+    'data': [
+        "views/hr_payroll_multicurrency_view.xml",
+    ],
+}
---
+++ hr_payroll_multicurrency/models/hr_payroll_multicurrency.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# © 2016 ClearCorp
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openerp import models, fields, api
+
+
+class HRContract(models.Model):
+    _inherit = 'hr.contract'
+
+    currency_id = fields.Many2one('res.currency')
+
+
+class HRPayslip(models.Model):
+    _inherit = 'hr.payslip'
+
+    currency_id = fields.Many2one('res.currency', compute='_compute_currency')
+
+    @api.depends('contract_id')
+    def _compute_currency(self):
+        self.currency_id = self.contract_id.currency_id
+
+    @api.multi
+    def process_sheet(self):
+        for payslip in self:
+            super(HRPayslip, self).process_sheet()
+            account_move = self.env['account.move'].search(
+                [('ref', '=', payslip.number)])
+            for account_move_line in account_move.line_id:
+                currency_id = self.company_id.currency_id
+                if account_move_line.credit > 0.0:
+                    account_move_line.credit = self.currency_id.with_context(
+                        {'date': account_move.date}
+                    ).compute(account_move_line.credit, currency_id)
+                if account_move_line.debit > 0.0:
+                    account_move_line.debit = self.currency_id.with_context(
+                        {'date': account_move.date}
+                    ).compute(account_move_line.debit, currency_id)
