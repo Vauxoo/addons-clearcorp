fed99316b8069eacf23b6b554c02b7fc38828be6
Glen Sojo <glen.sojo@clearcorp.co.cr>
2016-04-25 11:20:24 -0600

    [FIX] - account_move_rate: Fixed validation error in currency.

--- account_move_rate/account_move_rate.py
+++ account_move_rate/account_move_rate.py
@@ -13,20 +13,22 @@ class MoveLine(models.Model):
     @api.constrains('currency_id')
     def _get_rate_journal(self):
         for line in self:
-            if line.currency_id and line.currency_id.base:
-                rate = line.company_id.currency_id.rate_ids.search(
-                    [('name', '=', self.move_id.date)])
-                if not rate:
-                    raise Warning(
-                        _('There is no rate available for currency %s')
-                        % line.currency_id.name)
-            else:
-                rate = line.currency_id.rate_ids.search(
-                    [('name', '=', self.move_id.date)])
-                if not rate:
-                    raise Warning(
-                        _('There is no rate available for currency %s')
-                        % line.currency_id.name)
+            if line.currency_id:
+                if line.currency_id.base:
+                    rate = line.company_id.currency_id.rate_ids.search(
+                        [('name', '=', self.move_id.date)])
+                    if not rate:
+                        raise Warning(
+                            _('There is no rate available for currency %s')
+                            % line.currency_id.name)
+                else:
+                    rate = line.currency_id.rate_ids.search(
+                        [('name', '=', self.move_id.date)])
+                    if not rate:
+                        raise Warning(
+                            _('There is no rate available for currency %s')
+                            % line.currency_id.name)
+        return True
 
 
 class Move(models.Model):
