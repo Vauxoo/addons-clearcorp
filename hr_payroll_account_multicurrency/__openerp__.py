dcb6ff3f9d1a2b737d5407455771a1928314a97c
Rodney Vargas <rodney.vargas@clearcorp.co.cr>
2016-05-12 11:25:46 -0600

    [ADD]-hr_payroll_account_multicurrency: add joural items to payroll

---
+++ hr_payroll_account_multicurrency/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# © 2016 ClearCorp
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'HR Payroll Account MultiCurrency',
+    'summary': 'Create journal items in multi-currency',
+    'version': '8.0.1.0',
+    'category': 'HR',
+    'website': 'http://clearcorp.cr',
+    'author': 'ClearCorp',
+    'license': 'AGPL-3',
+    'sequence': 10,
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+    'depends': [
+        'hr_payroll_multicurrency',
+    ],
+}
