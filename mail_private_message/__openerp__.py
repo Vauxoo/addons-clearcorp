6e8904e2e052da3695d2d2ee2bd17e3a3d5669d2
Lesmed Gutierrez <lesmed.gutierrez@clearcorp.co.cr>
2016-05-23 15:56:03 -0600

    [FIX]-mail_private_message: Fix new headers and module structure in descriptor

--- mail_private_message/__openerp__.py
+++ mail_private_message/__openerp__.py
@@ -1,33 +1,19 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Addons modules by CLEARCORP S.A.
-#    Copyright (C) 2009-TODAY CLEARCORP S.A. (<http://clearcorp.co.cr>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2016 ClearCorp
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Mail Private Message',
-    'version': '1.0',
+    'version': '8.0.1.2',
     'category': 'Mail',
     'sequence': 10,
     'summary': """Private messages and notifications""",
     'author': 'ClearCorp',
     'website': 'http://clearcorp.cr',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'license': 'AGPL-3',
     'depends': ['mail', 'base'],
     'data': [
         'views/assets_backend.xml',
@@ -35,10 +21,4 @@
         'security/mail_private_message_security.xml',
     ],
     'qweb': ['static/src/xml/mail.xml'],
-    'test': [],
-    'demo': [],
-    'installable': True,
-    'auto_install': False,
-    'application': False,
-    'license': 'AGPL-3',
 }
