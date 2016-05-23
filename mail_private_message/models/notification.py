1a323bbc78696784ef51daa520b0687cb05c3ca8
Lesmed Gutierrez <lesmed.gutierrez@clearcorp.co.cr>
2016-05-23 15:54:57 -0600

    [FIX]-mail_private_messages: Fix bug then select diferents partners to notify

--- mail_private_message/models/notification.py
+++ mail_private_message/models/notification.py
@@ -1,23 +1,6 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2010-Today OpenERP SA (<http://www.openerp.com>)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>
-#
-##############################################################################
+# © 2016 ClearCorp
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp import models, api
 
@@ -43,6 +26,9 @@ class Notification(models.Model):
                                 self._cr, user.id, 'base.group_user'):
                             clean_partners.append(partner.id)
                 partners_to_notify = clean_partners
+        else:
+            partners_to_notify = self.env.context.get('partners_to_notify',
+                                                      partners_to_notify)
         return super(Notification, self)._notify(
             message_id, partners_to_notify=partners_to_notify,
             force_send=force_send, user_signature=user_signature)
