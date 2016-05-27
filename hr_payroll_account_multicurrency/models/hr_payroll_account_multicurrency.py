526b65ede073c18ca0afb9fcb24b771fa152add4
Glen Sojo <glen.sojo@clearcorp.co.cr>
2016-05-27 08:57:29 -0600

    Update hr_payroll_account_multicurrency.py

--- hr_payroll_account_multicurrency/models/hr_payroll_account_multicurrency.py
+++ hr_payroll_account_multicurrency/models/hr_payroll_account_multicurrency.py
@@ -12,23 +12,24 @@ class HRPayslip(models.Model):
     def process_sheet(self):
         for payslip in self:
             super(HRPayslip, self).process_sheet()
-            account_move = self.env['account.move'].search(
-                [('ref', '=', payslip.number)])
-            for account_move_line in account_move.line_id:
-                currency_id = self.company_id.currency_id
-                if account_move_line.credit > 0.0:
-                    account_move_line.write({
-                        'amount_currency': -1 * account_move_line.credit,
-                        'currency_id': payslip.currency_id.id
-                    })
-                    account_move_line.credit = self.currency_id.with_context(
-                        {'date': account_move.date}
-                    ).compute(account_move_line.credit, currency_id)
-                if account_move_line.debit > 0.0:
-                    account_move_line.write({
-                        'amount_currency': account_move_line.debit,
-                        'currency_id': payslip.currency_id.id
-                    })
-                    account_move_line.debit = self.currency_id.with_context(
-                        {'date': account_move.date}
-                    ).compute(account_move_line.debit, currency_id)
+            if payslip.currency_id:
+                account_move = self.env['account.move'].search(
+                    [('ref', '=', payslip.number)])
+                for account_move_line in account_move.line_id:
+                    currency_id = self.company_id.currency_id
+                    if account_move_line.credit > 0.0:
+                        account_move_line.write({
+                            'amount_currency': -1 * account_move_line.credit,
+                            'currency_id': payslip.currency_id.id
+                        })
+                        account_move_line.credit = self.currency_id.with_context(
+                            {'date': account_move.date}
+                        ).compute(account_move_line.credit, currency_id)
+                    if account_move_line.debit > 0.0:
+                        account_move_line.write({
+                            'amount_currency': account_move_line.debit,
+                            'currency_id': payslip.currency_id.id
+                        })
+                        account_move_line.debit = self.currency_id.with_context(
+                            {'date': account_move.date}
+                        ).compute(account_move_line.debit, currency_id)
