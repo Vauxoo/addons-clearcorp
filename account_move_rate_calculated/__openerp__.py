8bf2b2515ca809eb9ede9efd28ecd63eaffc1194
Glen Sojo <glen.sojo@clearcorp.co.cr>
2016-07-04 11:38:32 -0600

    [IMP] - account_move_rate_calculated: Module reviewed and improved.

--- account_move_rate_calculated/__openerp__.py
+++ account_move_rate_calculated/__openerp__.py
@@ -1,42 +1,20 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Addons modules by CLEARCORP S.A.
-#    Copyright (C) 2009-TODAY CLEARCORP S.A. (<http://clearcorp.co.cr>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2011 ClearCorp
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 {
-    "name" : 'Exchange Rate Calculated',
-    "version" : '1.0',
-    "author" : 'ClearCorp',
+    'name': 'Journal Item Rate Calculated',
+    'summary': 'Module summary',
+    'version': '9.0.1.0',
     'category': 'Accounting & Finance',
-    "description": """
-Exchange Rate Calculated:
-==========================
-This module permits, in manual account moves, it will compute the amount currency 
-and it applies the exchange rate for selected currency. I will assign the amount 
-in debit or credit field, if amount currency is negative, it will be credit and 
-if it is positve, it will be debit.
-""",
-    "website" : "http://clearcorp.co.cr",
-    "depends" : [
-                 'account',
-                 ],
-    'active': False,
-    'installable': True,    
+    'website': 'http://clearcorp.cr',
+    'author': 'ClearCorp',
     'license': 'AGPL-3',
+    'sequence': 31,
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+    'depends': [
+        'account',
+    ]
 }
--- account_move_rate_calculated/account_move_line.py
+++ account_move_rate_calculated/account_move_line.py
@@ -1,58 +1,33 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Addons modules by CLEARCORP S.A.
-#    Copyright (C) 2009-TODAY CLEARCORP S.A. (<http://clearcorp.co.cr>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2011 ClearCorp
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields, api
-import time
-import copy
+from openerp import models, api
 
 
-class accountMoveline(models.Model):
+class AccountMoveLine(models.Model):
 
     _inherit = 'account.move.line'
 
-    @api.onchange('amount_currency')
+    @api.onchange('amount_currency', 'currency_id')
     def onchange_amount_currency(self):
-        """This method provides convert the amount_currency to debit or credit
-        depends of currency selected. It only works in amount_currency to debit/credit.
-        In debit/credit to amount_currency it isn't implemented."""
+        """This method provides conversion of the amount_currency to debit
+        or credit depending on the currency selected."""
 
-        if(self.amount_currency):
-            # 1. Get currency for current company. 
-            # (The exchange rate for this case is from currency_company to currency_id)
+        if self.amount_currency and self.currency_id:
             company_currency = self.move_id.company_id.currency_id
-            # 2. Get date as string
-            if self.move_id and not self.move_id.date:
-                date = time.strftime('%Y-%m-%d')
-            else:
-                date = self.move_id.date
 
-            if self.amount_currency != 0 and self.currency_id:
-                # 3. Get amount_currency for today
-                currency_selected = self.currency_id.with_context(date=date)
-                exchange_amount = currency_selected.compute(self.amount_currency, company_currency)
-                # 4. Asign values to debit or credit
-                if exchange_amount >= 0:
-                    self.debit = exchange_amount
-                    self.credit = 0.0
-                else:
-                    self.credit = -1 * exchange_amount
-                    self.debit = 0.0
+            ctx = {}
+            if self.move_id.date:
+                ctx.update({'date': self.move_id.date})
+
+            exchange_amount = self.currency_id.with_context(ctx).compute(
+                abs(self.amount_currency), company_currency)
+
+            # 4. Assign values to debit or credit
+            if self.amount_currency >= 0:
+                self.debit = exchange_amount
+                self.credit = 0.0
+            else:
+                self.credit = exchange_amount
+                self.debit = 0.0
