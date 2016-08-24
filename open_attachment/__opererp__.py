91b390cdb750599a758f06dafa65bfee0b83c7f8
Antonio Hidalgo <antonio.hidalgo@clearcorp.co.cr>
2016-08-24 11:03:45 -0600

    [ADD] openerp file

---
+++ open_attachment/__opererp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# © 2016 ClearCorp
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Modify Attachment',
+    'version': '8.0.1.0',
+    'category': '',
+    'sequence': 10,
+    'summary': """Edit attached details of attached files.""",
+    'author': 'ClearCorp',
+    'website': 'http://clearcorp.cr',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'license': 'AGPL-3',
+    'depends': [],
+    'data': [],
+    'qweb': ['static/src/xml/modify_attachment.xml'],
+}
