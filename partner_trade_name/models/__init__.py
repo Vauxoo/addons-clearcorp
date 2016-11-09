a301d940cc241eddb25f09d20f801b5793195a4b
Ronald Rubí <ronald.rubi@clearcorp.co.cr>
2016-11-09 16:27:37 -0600

    [IMP] partner_trade_name: adjust module to version 9.0

--- partner_trade_name/models/__init__.py
+++ partner_trade_name/models/__init__.py
@@ -2,4 +2,4 @@
 # © 2016 ClearCorp
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import models
+from . import partner_trade_name
--- partner_trade_name/models/partner_trade_name.py
+++ partner_trade_name/models/partner_trade_name.py
@@ -1,25 +1,7 @@
-
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-# OpenERP, Open Source Management Solution
-# Addons modules by CLEARCORP S.A.
-# Copyright (C) 2009-TODAY CLEARCORP S.A. (<http://clearcorp.co.cr>).
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,related relationrelated relation
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2016 ClearCorp
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from openerp.osv import osv, fields, orm
 
 class ResPartner(orm.Model):
@@ -36,7 +18,7 @@ class ResPartner(orm.Model):
                     ' ' + '['+partner.trade_name+']'
             else:
                 partner_name += partner.name
-            res.append((obj_company.id, obj_company_name))
+            res.append((partner.id, partner_name))
         return res
 
     def name_search(self, cr, uid, name, args = None, operator = 'ilike',
