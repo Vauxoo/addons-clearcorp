9fa2dee55d81ac70fab3ef47cdb66bfcaeaa1547
David Ocampo <david.ocampo@clearcorp.co.cr>
2016-11-11 10:16:51 -0600

    [FIX] - account_invoice_partner_block: Fixed module
    Fixed: migrated from Odoo 8.0 to Odoo 9.0

--- account_invoice_partner_block/__init__.py
+++ account_invoice_partner_block/__init__.py
@@ -19,21 +19,5 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-{
-    'name' : 'Account Invoice Partner Block',
-    'version' : '1.0',
-    'author' : 'ClearCorp',
-    'category' : 'Tools',
-    'description': """
-Account Invoice Partner Block
-=============================
-Warning messages can be displayed for objects like account invoice.
-""",
-    'website': 'http://clearcorp.co.cr',
-    'depends' : ['base', 'sale_stock', 'purchase'],
-    'data': [],
-    'installable': True,
-    'active': False,
-    'license': 'AGPL-3',
 
-}
+from account_invoice_partner_block.models import account_invoice_partner_block
