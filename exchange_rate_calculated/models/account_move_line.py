133e1e8910a70dae6caf49783561e2b63ba42a15
Ronald Rubí <ronald.rubi@clearcorp.co.cr>
2016-11-14 12:10:31 -0600

    [FIX] exchange_rate_calculated: Fixes to pep8

--- exchange_rate_calculated/models/account_move_line.py
+++ exchange_rate_calculated/models/account_move_line.py
@@ -15,12 +15,14 @@ class AccountMoveline(models.Model):
     @api.onchange('amount_currency')
     def onchange_amount_currency(self):
         """This method provides convert the amount_currency to debit or credit
-        depends of currency selected. It only works in amount_currency to debit/credit.
-        In debit/credit to amount_currency it isn't implemented."""
+        depends of currency selected. It only works in amount_currency to
+        debit/credit. In debit/credit to amount_currency it isn't
+        implemented."""
 
         if(self.amount_currency):
-            # 1. Get currency for current company. 
-            # (The exchange rate for this case is from currency_company to currency_id)
+            #1. Get currency for current company.
+            #(The exchange rate for this case is from
+            #currency_company to currency_id)
             company_currency = self.move_id.company_id.currency_id
             # 2. Get date as string
             if self.move_id and not self.move_id.date:
@@ -31,7 +33,8 @@ class AccountMoveline(models.Model):
             if self.amount_currency != 0 and self.currency_id:
                 # 3. Get amount_currency for today
                 currency_selected = self.currency_id.with_context(date=date)
-                exchange_amount = currency_selected.compute(self.amount_currency, company_currency)
+                exchange_amount = currency_selected.compute(
+                    self.amount_currency, company_currency)
                 # 4. Asign values to debit or credit
                 if exchange_amount >= 0:
                     self.debit = exchange_amount
