7f7d647930dcd4099dfc836753704f7b3ca07521
Carlos Vásquez <carlos.vasquez@clearcorp.co.cr>
2018-04-04 16:09:13 -0600

    Moved account_invoice_discount and account_invoice_report to main dir to start migration.

--- account_invoice_discount/__manifest__.py
+++ account_invoice_discount/__manifest__.py
@@ -4,14 +4,14 @@
 {
     'name': 'Invoice discount',
     'summary': 'Adds a discount feature for invoice',
-    'version': '9.0.1.0',
+    'version': '1.0',
     'category': 'Accounting & Finance',
     'website': "http://clearcorp.cr",
     'author': 'ClearCorp',
     'license': 'AGPL-3',
     'sequence': 4,
     'application': False,
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'depends': ['account'],
     'data': [
