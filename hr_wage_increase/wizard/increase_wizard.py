7c3d9afc49884982459cf3b95a9edfb647737ae4
Carlos <carlos.1986.navarro@gmail.com>
2018-07-23 15:51:16 -0600

    [UPG]-Upgrade module hr_wage_increase to Odoo v10

--- hr_wage_increase/wizard/increase_wizard.py
+++ hr_wage_increase/wizard/increase_wizard.py
@@ -1,43 +1,57 @@
 # -*- coding: utf-8 -*-
-# © 2016 ClearCorp
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Addons modules by CLEARCORP S.A.
+#    Copyright (C) 2009-TODAY CLEARCORP S.A. (<http://clearcorp.co.cr>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 
-from openerp.osv import osv, fields
+from odoo import tools,models, fields, api,_
 from openerp.tools.translate import _
+from odoo.exceptions import UserError
 
-
-class WageIncreaseWizard(osv.TransientModel):
-
+class WageIncreaseWizard(models.TransientModel):
     _name = 'hr.wage.increase.increase.wizard'
-
-    def _check_amount_percentage(self, cr, uid, ids, context=None):
-        for wizard in self.browse(cr, uid, ids, context=context):
+    @api.one
+    @api.constrains('increase_type','amount_percentage')
+    def _check_amount_percentage(self):
+        for wizard in self:
             if wizard.increase_type == 'percentage':
-                if wizard.amount_percentage <= 0.0 or\
-                        wizard.amount_percentage > 100:
-                    return False
+                if wizard.amount_percentage <= 0.0 or wizard.amount_percentage > 100:
+                    raise Warning(_('Value must be greater than 0 and lower or equal than 100.'))
         return True
-
-    def _check_amount_fixed(self, cr, uid, ids, context=None):
-        for wizard in self.browse(cr, uid, ids, context=context):
+    @api.one
+    @api.constrains('increase_type','amount_fixed')
+    def _check_amount_fixed(self):
+        for wizard in self:
             if wizard.increase_type == 'fixed_amount':
                 if wizard.amount_fixed <= 0.0:
-                    return False
+                    raise Warning(_('Value must be greater than 0.'))
         return True
 
-    def process_wage_increase(self, cr, uid, ids, context=None):
-        if isinstance(ids, list):
-            ids = ids and ids[0]
-        wizard = self.browse(cr, uid, ids, context=context)
+    def process_wage_increase(self):
         try:
             contract_ids = []
-            for contract in wizard.contract_ids:
+            for contract in self.contract_ids:
                 new_wage = None
-                if wizard.increase_type == 'percentage':
-                    new_wage = contract.wage +\
-                        (contract.wage * wizard.amount_percentage / 100)
+                if self.increase_type == 'percentage':
+                    new_wage = contract.wage + (contract.wage * self.amount_percentage / 100)
                 else:
-                    new_wage = contract.wage + wizard.amount_fixed
+                    new_wage = contract.wage + self.amount_fixed
                 contract.write({'wage': new_wage})
                 contract_ids.append(contract.id)
             return {
@@ -46,35 +60,13 @@ class WageIncreaseWizard(osv.TransientModel):
                 'view_mode': 'tree,form',
                 'res_model': 'hr.contract',
                 'view_id': False,
-                'context': context,
                 'domain': "[('id','in',%s)]" % contract_ids,
                 'type': 'ir.actions.act_window',
             }
         except:
-            raise osv.except_osv(
-                _('Error Processing Contracts'),
-                _('An error occurred while processing the contracts'))
+            raise Warning(_('An error occurred while processing the contracts'))
 
-    _columns = {
-        'increase_type': fields.selection(
-            [('percentage', 'Percentage (%)'),
-             ('fixed_amount', 'Fixed Amount')
-             ],
-            string='Increase Type', required=True),
-        'amount_percentage': fields.float('Increase Percentage'),
-        'amount_fixed': fields.float('Amount', digits=(16, 2)),
-        'contract_ids': fields.many2many('hr.contract', string='Contracts',
-                                         required=True),
-    }
-
-    _defaults = {
-        'increase_type': 'fixed_amount',
-    }
-
-    _constraints = [
-        (_check_amount_percentage,
-         'Value must be greater than 0 and lower or equal than 100.',
-         ['amount_percentage']),
-        (_check_amount_fixed, 'Value must be greater than 0.',
-         ['amount_fixed'])
-    ]
+    increase_type= fields.Selection([('percentage','Percentage (%)'),('fixed_amount','Fixed Amount')],default='fixed_amount',string='Increase Type', required=True)
+    amount_percentage=fields.Float('Increase Percentage')
+    amount_fixed= fields.Float('Amount', digits=(16,2))
+    contract_ids= fields.Many2many('hr.contract', string='Contracts', required=True)
