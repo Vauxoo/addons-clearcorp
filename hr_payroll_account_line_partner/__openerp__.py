b0204eb8ba10263b6e4f921cadb031d99a9b8ef0
Carlos <carlos.1986.navarro@gmail.com>
2018-07-23 16:00:34 -0600

    [UPG]-Upgrade module hr_payroll_account_line_partner to Odoo v10

--- hr_payroll_account_line_partner/__openerp__.py
+++ hr_payroll_account_line_partner/__openerp__.py
@@ -1,22 +1,48 @@
 # -*- coding: utf-8 -*-
-# © 2016 ClearCorp
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Addons modules by CLEARCORP S.A.
+#    Copyright (C) 2009-TODAY CLEARCORP S.A. (<http://clearcorp.co.cr>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 
 {
-    'name': 'Payroll Account Line Partner',
-    'version': '9.0.1.0.2',
-    'author': 'ClearCorp',
-    'summary': ''' Add partners and employees in payroll entries''',
+    'name' : 'Payroll Account Line Partner',
+    'version' : '1.0',
+    'author' : 'ClearCorp',
+    'complexity': 'easy',
+    'description': ''' Add partners and employees in payroll entries
+''',
     'category': 'Human Resources',
-    'website': 'http://clearcorp.cr',
-    'application': False,
-    'installable': False,
+    'sequence': 10,
+    'website' : 'http://clearcorp.co.cr',
+    'images' : [],
+    'depends' : [
+                 'hr_payroll_account',
+                 ],
+    'data' : [
+              'view/hr_payroll_account_line_partner_view.xml',
+              ],
+    'init_xml' : [],
+    'demo_xml' : [],
+    'update_xml' : [],
+    'test' : [],
     'auto_install': False,
+    'application': False,
+    'installable': True,
     'license': 'AGPL-3',
-    'depends': [
-        'hr_payroll_account',
-    ],
-    'data': [
-        'view/hr_payroll_account_line_partner_view.xml',
-    ],
 }
