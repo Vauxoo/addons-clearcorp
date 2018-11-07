00eb1481175ef29c471f6e996afe8781f329f793
Carlos <carlos.1986.navarro@gmail.com>
2018-11-07 13:05:32 -0600

    [FIX]-purchase_order_discount-Change function of discount calculate

--- purchase_order_discount/models/purchase_order_discount.py
+++ purchase_order_discount/models/purchase_order_discount.py
@@ -24,6 +24,7 @@ class PurchaseOrderLine(models.Model):
             line.update({
                 'price_tax': taxes['total_included'] - taxes['total_excluded'],
                 'price_total': taxes['total_included'],
+                'price_subtotal': taxes['total_excluded'],
             })
 
     discount = fields.Float(
@@ -46,16 +47,21 @@ class PurchaseOrder(models.Model):
     def _amount_all(self):
         super(PurchaseOrder, self)._amount_all()
         for order in self:
-            amount_discount = 0.0
+            amount_untaxed = amount_tax =amount_discount= 0.0
             for line in order.order_line:
-                amount_discount += ((line.discount / 100) *
-                                    line.price_subtotal)
-            amount_total = order.amount_untaxed + order.amount_tax - \
-                amount_discount
+                amount_untaxed += line.price_subtotal
+                amount_discount +=   (line.price_unit*line.product_qty) - (line.price_subtotal)
+                if order.company_id.tax_calculation_rounding_method == 'round_globally':
+                    price = line.price_unit - (line.discount / 100 * line.price_unit)
+                    taxes = line.taxes_id.compute_all(price, line.order_id.currency_id, line.product_qty, product=line.product_id, partner=line.order_id.partner_id)
+                    amount_tax += sum(t.get('amount', 0.0) for t in taxes.get('taxes', []))
+                else:
+                    amount_tax += line.price_tax
             order.update({
-                'amount_discount': order.currency_id.round(
-                    amount_discount),
-                'amount_total': order.currency_id.round(amount_total),
+                'amount_untaxed': order.currency_id.round(amount_untaxed),
+                'amount_tax': order.currency_id.round(amount_tax),
+                'amount_total': amount_untaxed + amount_tax,
+                'amount_discount': order.currency_id.round(amount_discount)
             })
 
     amount_untaxed = fields.Monetary(string='Subtotal')
