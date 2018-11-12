90dbda0705effea5d910d7864f9b82193412d52e
Carlos Vásquez <carlos.vasquez@clearcorp.co.cr>
2018-11-11 23:40:11 -0600

    Fixed bad import and bad commend on stock_requisition_group.

--- stock_requisition_group/__init__.py
+++ stock_requisition_group/__init__.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
 #    Copyright (C) 2009-TODAY CLEARCORP S.A. (<http://clearcorp.co.cr>).
 
-import purchase_requisition
-import procurement_order
-import purchase_order
-import stock_move
+from . import purchase_requisition, procurement_order, purchase_order, stock_move
--- stock_requisition_group/purchase_order.py
+++ stock_requisition_group/purchase_order.py
@@ -40,48 +40,46 @@ class PurchaseOrderLine(models.Model):
 
     _inherit = 'purchase.order.line'
 
-"""
-    @api.one
-    def match_procurement(self):
-        if self.requisition_id and not self.procurement_ids:
-            if not self.product_id:
-                return True
-            self._cr.execute("""SELECT prl.id, prl.product_qty, prl.procurement_id,
-(SELECT COALESCE(SUM(rql.product_qty), 0.0)
- FROM purchase_requisition_line AS rql,
-   procurement_order AS pro
- WHERE rql.id = prl.id
- AND pro.purchase_line_id IS NOT NULL
- AND rql.procurement_id = pro.id) AS assigned
-FROM purchase_requisition_line AS prl
-WHERE prl.requisition_id = %s
-AND prl.product_id = %s""", [self.requisition_id.id,
-                             self.product_id.id])
-            possible_matches = self._cr.dictfetchall()
-            # Find the perfect match
-            for match in possible_matches:
-                if match['product_qty'] - \
-                        match['assigned'] == self.product_qty:
-                    self.procurement_ids = [(4, match['procurement_id'])]
-                    return True
-            # Fit it in place with a posible match
-            diff = self.product_qty
-            for match in possible_matches:
-                if (match['product_qty'] -
-                        match['assigned'] <= diff) and \
-                        (match['product_qty'] - match['assigned'] != 0):
-                    diff = diff - (match['product_qty'] -
-                                   match['assigned'])
-                    # Assign the match
-                    self.procurement_ids = [
-                        (4, match['procurement_id'])]
-            # Raise an error if no match is found
-            if diff > 0:
-                raise Warning(_('It was unable to be processed due '
-                                'to the error in the match of quantities.'))
-            return True
-
-    requisition_id = fields.Many2one(
-        'purchase.requisition', related='order_id.requisition_id',
-        string='Requisition', readonly=True)
-"""
+#     @api.one
+#     def match_procurement(self):
+#         if self.requisition_id and not self.procurement_ids:
+#             if not self.product_id:
+#                 return True
+#             self._cr.execute("""SELECT prl.id, prl.product_qty, prl.procurement_id,
+# (SELECT COALESCE(SUM(rql.product_qty), 0.0)
+#  FROM purchase_requisition_line AS rql,
+#    procurement_order AS pro
+#  WHERE rql.id = prl.id
+#  AND pro.purchase_line_id IS NOT NULL
+#  AND rql.procurement_id = pro.id) AS assigned
+# FROM purchase_requisition_line AS prl
+# WHERE prl.requisition_id = %s
+# AND prl.product_id = %s""", [self.requisition_id.id,
+#                              self.product_id.id])
+#             possible_matches = self._cr.dictfetchall()
+#             # Find the perfect match
+#             for match in possible_matches:
+#                 if match['product_qty'] - \
+#                         match['assigned'] == self.product_qty:
+#                     self.procurement_ids = [(4, match['procurement_id'])]
+#                     return True
+#             # Fit it in place with a posible match
+#             diff = self.product_qty
+#             for match in possible_matches:
+#                 if (match['product_qty'] -
+#                         match['assigned'] <= diff) and \
+#                         (match['product_qty'] - match['assigned'] != 0):
+#                     diff = diff - (match['product_qty'] -
+#                                    match['assigned'])
+#                     # Assign the match
+#                     self.procurement_ids = [
+#                         (4, match['procurement_id'])]
+#             # Raise an error if no match is found
+#             if diff > 0:
+#                 raise Warning(_('It was unable to be processed due '
+#                                 'to the error in the match of quantities.'))
+#             return True
+#
+#     requisition_id = fields.Many2one(
+#         'purchase.requisition', related='order_id.requisition_id',
+#         string='Requisition', readonly=True)
