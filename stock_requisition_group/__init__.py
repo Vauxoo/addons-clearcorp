01799a3d5da6857f1ea79a9a8d95d73bfeb978ed
Carlos Vásquez <carlos.vasquez@clearcorp.co.cr>
2018-11-12 23:26:38 -0600

    New stock_requisition_group models, much less modification to get requisition consolidation in v11.

--- stock_requisition_group/__init__.py
+++ stock_requisition_group/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 #    Copyright (C) 2009-TODAY CLEARCORP S.A. (<http://clearcorp.co.cr>).
 
-from . import purchase_requisition, procurement_order, purchase_order, stock_move
+from . import models
